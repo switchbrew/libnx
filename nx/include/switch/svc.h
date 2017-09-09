778d5fc4fb306f59a430c7ebfea0beaa4d159a53
yellows8 <yellows8@users.noreply.github.com>
2017-09-09 12:10:45 -0400

    Added proper svcQueryIoMapping.

--- nx/include/switch/svc.h
+++ nx/include/switch/svc.h
@@ -24,5 +24,6 @@ Result svcConnectToNamedPort(Handle* session, const char* name);
 Result svcBreak(u32 BreakReason, u64 inval1, u64 inval2);
 Result svcAcceptSession(Handle *session_handle, Handle port_handle);
 Result svcReplyAndReceive(s32* index, const Handle* handles, s32 handleCount, Handle replyTarget, u64 timeout);
+Result svcQueryIoMapping(u64* virtaddr, u64 physaddr, u64 size);
 Result svcManageNamedPort(Handle* portServer, const char* name, s32 maxSessions);
 
--- nx/source/svc.s
+++ nx/source/svc.s
@@ -53,6 +53,14 @@ SVC_BEGIN svcReplyAndReceive
 	ret
 SVC_END
 
+SVC_BEGIN svcQueryIoMapping
+	str x0, [sp, #-16]!
+	svc 0x55
+	ldr x2, [sp], #16
+	str x1, [x2]
+	ret
+SVC_END
+
 SVC_BEGIN svcManageNamedPort
 	str x0, [sp, #-16]!
 	svc 0x71
