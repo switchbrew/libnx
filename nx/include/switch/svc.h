59290a5b87fda5d77a45053b2d55d79ca5e33331
yellows8 <yellows8@users.noreply.github.com>
2017-09-09 16:27:55 -0400

    Added svcQueryPhysicalAddress.

--- nx/include/switch/svc.h
+++ nx/include/switch/svc.h
@@ -25,5 +25,6 @@ Result svcSendSyncRequest(Handle session);
 Result svcBreak(u32 BreakReason, u64 inval1, u64 inval2);
 Result svcAcceptSession(Handle *session_handle, Handle port_handle);
 Result svcReplyAndReceive(s32* index, const Handle* handles, s32 handleCount, Handle replyTarget, u64 timeout);
+Result svcQueryPhysicalAddress(u64* out, u64 virtaddr);//3 output u64s are written to out.
 Result svcQueryIoMapping(u64* virtaddr, u64 physaddr, u64 size);
 Result svcManageNamedPort(Handle* portServer, const char* name, s32 maxSessions);
