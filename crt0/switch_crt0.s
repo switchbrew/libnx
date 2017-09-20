1630fc16404c56ff41007652f83b8c7c2ab85734
fincs <fincs.alt1@gmail.com>
2017-09-21 01:12:11 +0200

    Implement proper .dynamic section parsing

--- crt0/switch_crt0.s
+++ crt0/switch_crt0.s
@@ -3,55 +3,39 @@
 
 _start:
     bl startup
-
-    .word 0x454d4f48
-    .word 0x57455242
+    .ascii "HOMEBREW"
 
 startup:
     // get aslr base
     sub  x28, x30, #4
 
     // clear .bss
-    ldr  x0, =__bss_start__
-    ldr  x1, =__bss_end__
+    adrp x0, __bss_start__
+    adrp x1, __bss_end__
+    add  x0, x0, #:lo12:__bss_start__
+    add  x1, x1, #:lo12:__bss_end__
     sub  x1, x1, x0  // calculate size
     add  x1, x1, #7  // round up to 8
     bic  x1, x1, #7
-    mov  x2, #0
-    add  x0, x0, x28 // relocate ptr
 
 bss_loop: 
-    str  x2, [x0], #8
+    str  xzr, [x0], #8
     subs x1, x1, #8
     bne  bss_loop
 
-    // relocate .got
-    ldr  x0, =__got_start__
-    ldr  x1, =__got_end__
-    sub  x1, x1, x0  // calculate size
-    add  x1, x1, #7  // round up to 8
-    bic  x1, x1, #7
-    add  x0, x0, x28 // relocate ptr
-
-got_loop: 
-    ldr  x2, [x0]
-    add  x2, x2, x28
-    str  x2, [x0], #8
-    subs x1, x1, #8
-    bne  got_loop
-
+    // process .dynamic section
     mov  x0, x28
-    ldr  x3, =initSystem
-    add  x3, x3, x28
-    blr  x3
+    adrp x1, _DYNAMIC
+    add  x1, x1, #:lo12:_DYNAMIC
+    bl   __nx_dynamic
 
+    // initialize system
+    mov  x0, x28
+    bl   __nx_init
+
+    // call entrypoint
     mov  x0, #0 // argc
     mov  x1, #0 // argv
-
-    ldr  x3, =main
-    add  x3, x3, x28
-    ldr  x2, =__nx_exit
-    add  x2, x2, x28
-    mov  x30, x2
-    br  x3
-
+    adrp x30, __nx_exit
+    add  x30, x30, #:lo12:__nx_exit
+    b    main
