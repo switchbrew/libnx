718d55f45a89bacef007f8e43288c9c87010b903
plutoo <plutoo@universe>
2017-09-24 19:49:24 +0200

    Added sm service commands

--- nx/include/switch/services/sm.h
+++ nx/include/switch/services/sm.h
@@ -1,3 +1,5 @@
 bool smHasInitialized();
 Result smInitialize();
 Result smGetService(Handle* handle_out, const char* name);
+Result smRegisterService(Handle* handle_out, const char* name, bool is_light, int max_sessions);
+Result smUnregisterService(const char* name);
--- nx/source/services/sm.c
+++ nx/source/services/sm.c
@@ -47,7 +47,7 @@ Result smInitialize() {
     return rc;
 }
 
-Result smGetService(Handle* handle_out, const char* name) {
+static u64 _EncodeName(const char* name) {
     u64 name_encoded = 0;
 
     size_t i;
@@ -58,6 +58,10 @@ Result smGetService(Handle* handle_out, const char* name) {
         name_encoded |= ((u64) name[i]) << (8*i);
     }
 
+    return name_encoded;
+}
+
+Result smGetService(Handle* handle_out, const char* name) {
     IpcCommand c;
     ipcInitialize(&c);
 
@@ -72,7 +76,7 @@ Result smGetService(Handle* handle_out, const char* name) {
 
     raw->magic = SFCI_MAGIC;
     raw->cmd_id = 1;
-    raw->service_name = name_encoded;
+    raw->service_name = _EncodeName(name);
 
     Result rc = ipcDispatch(g_smHandle);
 
@@ -90,6 +94,81 @@ Result smGetService(Handle* handle_out, const char* name) {
         if (R_SUCCEEDED(rc)) {
             *handle_out = r.Handles[0];
         }
+    }
+
+    return rc;
+}
+
+Result smRegisterService(Handle* handle_out, const char* name, bool is_light, int max_sessions) {
+    IpcCommand c;
+    ipcInitialize(&c);
+
+    struct {
+        u64 magic;
+        u64 cmd_id;
+        u64 service_name;
+        u32 is_light;
+        u32 max_sessions;
+    } *raw;
+
+    raw = ipcPrepareHeader(&c, sizeof(*raw));
+
+    raw->magic = SFCI_MAGIC;
+    raw->cmd_id = 2;
+    raw->service_name = _EncodeName(name);
+    raw->is_light = !!is_light;
+    raw->max_sessions = max_sessions;
+
+    Result rc = ipcDispatch(g_smHandle);
+
+    if (R_SUCCEEDED(rc)) {
+        IpcCommandResponse r;
+        ipcParseResponse(&r);
+
+        struct {
+            u64 magic;
+            u64 result;
+        } *resp = r.Raw;
+
+        rc = resp->result;
+
+        if (R_SUCCEEDED(rc)) {
+            *handle_out = r.Handles[0];
+        }
+    }
+
+    return rc;
+}
+
+Result smUnregisterService(const char* name) {
+    IpcCommand c;
+    ipcInitialize(&c);
+
+    struct {
+        u64 magic;
+        u64 cmd_id;
+        u64 service_name;
+        u64 reserved;
+    } *raw;
+
+    raw = ipcPrepareHeader(&c, sizeof(*raw));
+
+    raw->magic = SFCI_MAGIC;
+    raw->cmd_id = 3;
+    raw->service_name = _EncodeName(name);
+
+    Result rc = ipcDispatch(g_smHandle);
+
+    if (R_SUCCEEDED(rc)) {
+        IpcCommandResponse r;
+        ipcParseResponse(&r);
+
+        struct {
+            u64 magic;
+            u64 result;
+        } *resp = r.Raw;
+
+        rc = resp->result;
     }
 
     return rc;
