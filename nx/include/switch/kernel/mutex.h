cbd367c8a0c814d5666096ce84925d8f749bce31
fincs <fincs.alt1@gmail.com>
2018-01-28 17:27:56 +0100

    Document kernel/: detect.h, jit.h, mutex.h, random.h, rwlock.h

--- nx/include/switch/kernel/mutex.h
+++ nx/include/switch/kernel/mutex.h
@@ -1,19 +1,43 @@
-// Copyright 2017 plutoo
+/**
+ * @file mutex.h
+ * @brief Mutex synchronization primitive.
+ * @author plutoo
+ * @copyright libnx Authors
+ */
 #pragma once
 #include <sys/lock.h>
 #include "../types.h" // not needed in this file, still including it
 
+/// Mutex datatype, defined in newlib.
 typedef _LOCK_T Mutex;
+/// Recursive mutex datatype, defined in newlib.
 typedef _LOCK_RECURSIVE_T RMutex;
 
+/**
+ * @brief Initializes a mutex.
+ * @param m Mutex object.
+ */
 static inline void mutexInit(Mutex* m)
 {
     *m = 0;
 }
 
+/**
+ * @brief Locks a mutex.
+ * @param m Mutex object.
+ */
 void mutexLock(Mutex* m);
+
+/**
+ * @brief Unlocks a mutex.
+ * @param m Mutex object.
+ */
 void mutexUnlock(Mutex* m);
 
+/**
+ * @brief Initializes a recursive mutex.
+ * @param m Recursive mutex object.
+ */
 static inline void rmutexInit(RMutex* m)
 {
     m->lock = 0;
@@ -21,5 +45,14 @@ static inline void rmutexInit(RMutex* m)
     m->counter = 0;
 }
 
+/**
+ * @brief Locks a recursive mutex.
+ * @param m Recursive mutex object.
+ */
 void rmutexLock(RMutex* m);
+
+/**
+ * @brief Unlocks a recursive mutex.
+ * @param m Recursive mutex object.
+ */
 void rmutexUnlock(RMutex* m);
--- nx/include/switch/kernel/random.h
+++ nx/include/switch/kernel/random.h
@@ -1,6 +1,21 @@
-// Copyright 2018 plutoo
+/**
+ * @file random.h
+ * @brief OS-seeded pseudo-random number generation support (ChaCha algorithm).
+ * @author plutoo
+ * @copyright libnx Authors
+ */
 #pragma once
 #include "../types.h"
 
-void randomGet(u8* buf, size_t len);
+/**
+ * @brief Fills a buffer with random data.
+ * @param buf Pointer to the buffer.
+ * @param len Size of the buffer in bytes.
+ */
+void randomGet(void* buf, size_t len);
+
+/**
+ * @brief Returns a random 64-bit value.
+ * @return Random value.
+ */
 u64  randomGet64(void);
