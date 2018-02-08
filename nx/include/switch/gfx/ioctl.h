a09b60a315e69b88ce53a98831d3c8661fd9fce5
TuxSH <tuxsh@sfr.fr>
2018-02-08 01:05:19 +0100

    Fix NV ioctl nameclashes...
    ...with bsd ioctls (coming soon) & C++ stdlib

--- nx/include/switch/gfx/ioctl.h
+++ nx/include/switch/gfx/ioctl.h
@@ -2,46 +2,46 @@
 
 //The below defines are from Linux kernel ioctl.h.
 
-#define _IOC_NRBITS	8
-#define _IOC_TYPEBITS	8
-#define _IOC_SIZEBITS	14
-#define _IOC_DIRBITS	2
+#define _NV_IOC_NRBITS	8
+#define _NV_IOC_TYPEBITS	8
+#define _NV_IOC_SIZEBITS	14
+#define _NV_IOC_DIRBITS	2
 
-#define _IOC_NRMASK	((1 << _IOC_NRBITS)-1)
-#define _IOC_TYPEMASK	((1 << _IOC_TYPEBITS)-1)
-#define _IOC_SIZEMASK	((1 << _IOC_SIZEBITS)-1)
-#define _IOC_DIRMASK	((1 << _IOC_DIRBITS)-1)
+#define _NV_IOC_NRMASK	((1 << _NV_IOC_NRBITS)-1)
+#define _NV_IOC_TYPEMASK	((1 << _NV_IOC_TYPEBITS)-1)
+#define _NV_IOC_SIZEMASK	((1 << _NV_IOC_SIZEBITS)-1)
+#define _NV_IOC_DIRMASK	((1 << _NV_IOC_DIRBITS)-1)
 
-#define _IOC_NRSHIFT	0
-#define _IOC_TYPESHIFT	(_IOC_NRSHIFT+_IOC_NRBITS)
-#define _IOC_SIZESHIFT	(_IOC_TYPESHIFT+_IOC_TYPEBITS)
-#define _IOC_DIRSHIFT	(_IOC_SIZESHIFT+_IOC_SIZEBITS)
+#define _NV_IOC_NRSHIFT	0
+#define _NV_IOC_TYPESHIFT	(_NV_IOC_NRSHIFT+_NV_IOC_NRBITS)
+#define _NV_IOC_SIZESHIFT	(_NV_IOC_TYPESHIFT+_NV_IOC_TYPEBITS)
+#define _NV_IOC_DIRSHIFT	(_NV_IOC_SIZESHIFT+_NV_IOC_SIZEBITS)
 
 /*
  * Direction bits.
  */
-#define _IOC_NONE	0U
-#define _IOC_WRITE	1U
-#define _IOC_READ	2U
+#define _NV_IOC_NONE	0U
+#define _NV_IOC_WRITE	1U
+#define _NV_IOC_READ	2U
 
-#define _IOC(dir,type,nr,size) \
-	(((dir)  << _IOC_DIRSHIFT) | \
-	 ((type) << _IOC_TYPESHIFT) | \
-	 ((nr)   << _IOC_NRSHIFT) | \
-	 ((size) << _IOC_SIZESHIFT))
+#define _NV_IOC(dir,type,nr,size) \
+	(((dir)  << _NV_IOC_DIRSHIFT) | \
+	 ((type) << _NV_IOC_TYPESHIFT) | \
+	 ((nr)   << _NV_IOC_NRSHIFT) | \
+	 ((size) << _NV_IOC_SIZESHIFT))
 
 /* used to create numbers */
-#define _IO(type,nr)		_IOC(_IOC_NONE,(type),(nr),0)
-#define _IOR(type,nr,size)	_IOC(_IOC_READ,(type),(nr),sizeof(size))
-#define _IOW(type,nr,size)	_IOC(_IOC_WRITE,(type),(nr),sizeof(size))
-#define _IOWR(type,nr,size)	_IOC(_IOC_READ|_IOC_WRITE,(type),(nr),sizeof(size))
+#define _NV_IO(type,nr)		_NV_IOC(_NV_IOC_NONE,(type),(nr),0)
+#define _NV_IOR(type,nr,size)	_NV_IOC(_NV_IOC_READ,(type),(nr),sizeof(size))
+#define _NV_IOW(type,nr,size)	_NV_IOC(_NV_IOC_WRITE,(type),(nr),sizeof(size))
+#define _NV_IOWR(type,nr,size)	_NV_IOC(_NV_IOC_READ|_NV_IOC_WRITE,(type),(nr),sizeof(size))
 
 /* used to decode ioctl numbers.. */
-#define _IOC_DIR(nr)		(((nr) >> _IOC_DIRSHIFT) & _IOC_DIRMASK)
-#define _IOC_TYPE(nr)		(((nr) >> _IOC_TYPESHIFT) & _IOC_TYPEMASK)
-#define _IOC_NR(nr)		(((nr) >> _IOC_NRSHIFT) & _IOC_NRMASK)
-#define _IOC_SIZE(nr)		(((nr) >> _IOC_SIZESHIFT) & _IOC_SIZEMASK)
+#define _NV_IOC_DIR(nr)		(((nr) >> _NV_IOC_DIRSHIFT) & _NV_IOC_DIRMASK)
+#define _NV_IOC_TYPE(nr)		(((nr) >> _NV_IOC_TYPESHIFT) & _NV_IOC_TYPEMASK)
+#define _NV_IOC_NR(nr)		(((nr) >> _NV_IOC_NRSHIFT) & _NV_IOC_NRMASK)
+#define _NV_IOC_SIZE(nr)		(((nr) >> _NV_IOC_SIZESHIFT) & _NV_IOC_SIZEMASK)
 
-#define _in_
-#define _out_
-#define _inout_
+#define __nv_in
+#define __nv_out
+#define __nv_inout
--- nx/source/gfx/ioctl/nvmap.c
+++ nx/source/gfx/ioctl/nvmap.c
@@ -9,14 +9,14 @@ Result nvioctlNvmap_Create(u32 fd, u32 size, u32 *nvmap_handle) {
     Result rc=0;
 
     struct {
-        _in_  u32 size;
-        _out_ u32 handle;
+        __nv_in  u32 size;
+        __nv_out u32 handle;
     } data;
 
     memset(&data, 0, sizeof(data));
     data.size = size;
 
-    rc = nvIoctl(fd, _IOWR(0x01, 0x01, data), &data);
+    rc = nvIoctl(fd, _NV_IOWR(0x01, 0x01, data), &data);
 
     if (R_SUCCEEDED(rc)) {
         *nvmap_handle = data.handle;
@@ -29,14 +29,14 @@ Result nvioctlNvmap_FromId(u32 fd, u32 id, u32 *nvmap_handle) {
     Result rc=0;
 
     struct {
-        _in_  u32 id;
-        _out_ u32 handle;
+        __nv_in  u32 id;
+        __nv_out u32 handle;
     } data;
 
     memset(&data, 0, sizeof(data));
     data.id = id;
 
-    rc = nvIoctl(fd, _IOWR(0x01, 0x03, data), &data);
+    rc = nvIoctl(fd, _NV_IOWR(0x01, 0x03, data), &data);
 
     if (R_SUCCEEDED(rc)) {
         *nvmap_handle = data.handle;
@@ -47,13 +47,13 @@ Result nvioctlNvmap_FromId(u32 fd, u32 id, u32 *nvmap_handle) {
 
 Result nvioctlNvmap_Alloc(u32 fd, u32 nvmap_handle, u32 heapmask, u32 flags, u32 align, u8 kind, void* addr) {
     struct {
-        _in_ u32 handle;
-        _in_ u32 heapmask;
-        _in_ u32 flags;    // (0=read-only, 1=read-write)
-        _in_ u32 align;
-        _in_ u8  kind;
+        __nv_in u32 handle;
+        __nv_in u32 heapmask;
+        __nv_in u32 flags;    // (0=read-only, 1=read-write)
+        __nv_in u32 align;
+        __nv_in u8  kind;
         u8       pad[7];
-        _in_ u64 addr;
+        __nv_in u64 addr;
     } data;
 
     memset(&data, 0, sizeof(data));
@@ -64,21 +64,21 @@ Result nvioctlNvmap_Alloc(u32 fd, u32 nvmap_handle, u32 heapmask, u32 flags, u32
     data.kind = kind;
     data.addr = (u64)addr;
 
-    return nvIoctl(fd, _IOWR(0x01, 0x04, data), &data);
+    return nvIoctl(fd, _NV_IOWR(0x01, 0x04, data), &data);
 }
 
 Result nvioctlNvmap_GetId(u32 fd, u32 nvmap_handle, u32 *id) {
     Result rc=0;
 
     struct {
-        _out_ u32 id;
-        _in_  u32 handle;
+        __nv_out u32 id;
+        __nv_in  u32 handle;
     } data;
 
     memset(&data, 0, sizeof(data));
     data.handle = nvmap_handle;
 
-    rc = nvIoctl(fd, _IOWR(0x01, 0x0E, data), &data);
+    rc = nvIoctl(fd, _NV_IOWR(0x01, 0x0E, data), &data);
 
     if (R_SUCCEEDED(rc)) {
         *id = data.id;
