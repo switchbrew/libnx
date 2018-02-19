45e998de855512610029a6e9d43f15e3dce0344b
Mike H <mike.hexkyz@gmail.com>
2018-02-19 20:44:27 +0000

    Add alignment note in header file

--- nx/include/switch/services/audout.h
+++ nx/include/switch/services/audout.h
@@ -29,7 +29,7 @@ typedef struct AudioOutBuffer AudioOutBuffer;
 struct AudioOutBuffer
 {
     AudioOutBuffer* next;       ///< Next buffer.
-    void* buffer;               ///< Sample buffer.
+    void* buffer;               ///< Sample buffer (aligned to 0x1000 bytes).
     u64 buffer_size;            ///< Sample buffer size.
     u64 data_size;              ///< Size of data inside the buffer.
     u64 data_offset;            ///< Offset of data inside the buffer.
