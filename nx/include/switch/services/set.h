f1fc00c516e84028a535e5f3b2d5d142325c51a0
yellows8 <yellows8@users.noreply.github.com>
2018-02-26 15:17:21 -0500

    Updated comments for SetLanguage.

--- nx/include/switch/services/set.h
+++ nx/include/switch/services/set.h
@@ -16,20 +16,20 @@ typedef enum {
 typedef enum
 {
     SetLanguage_JA =    0,  ///< Japanese
-    SetLanguage_ENUS =  1,  ///< US English
+    SetLanguage_ENUS =  1,  ///< US English ("AmericanEnglish")
     SetLanguage_FR =    2,  ///< French
     SetLanguage_DE =    3,  ///< German
     SetLanguage_IT =    4,  ///< Italian
     SetLanguage_ES =    5,  ///< Spanish
-    SetLanguage_ZHCN =  6,  ///< Simplified Chinese
+    SetLanguage_ZHCN =  6,  ///< Simplified Chinese ("Chinese")
     SetLanguage_KO =    7,  ///< Korean
     SetLanguage_NL =    8,  ///< Dutch
-    SetLanguage_PT =    9,  ///< Portugese
+    SetLanguage_PT =    9,  ///< Portuguese
     SetLanguage_RU =    10, ///< Russian
-    SetLanguage_ZHTW =  11, ///< Traditional Chinese
-    SetLanguage_ENGB =  12, ///< GB English
-    SetLanguage_FRCA =  13, ///< CA French
-    SetLanguage_ES419 = 14, ///< {?} Spanish
+    SetLanguage_ZHTW =  11, ///< Traditional Chinese ("Taiwanese")
+    SetLanguage_ENGB =  12, ///< GB English ("BritishEnglish")
+    SetLanguage_FRCA =  13, ///< CA French ("CanadianFrench")
+    SetLanguage_ES419 = 14, ///< "LatinAmericanSpanish"
 } SetLanguage;
 
 Result setInitialize(void);
