20a549b46bc8bab73d59c681ed5139049126de82
yellows8 <yellows8@users.noreply.github.com>
2018-03-01 22:55:17 -0500

    Clear allocated tmem.

--- nx/source/kernel/tmem.c
+++ nx/source/kernel/tmem.c
@@ -1,4 +1,5 @@
 // Copyright 2017 plutoo
+#include <string.h>
 #include <malloc.h>
 #include "types.h"
 #include "result.h"
@@ -19,6 +20,8 @@ Result tmemCreate(TransferMemory* t, size_t size, Permission perm)
     if (t->src_addr == NULL) {
         rc = MAKERESULT(Module_Libnx, LibnxError_OutOfMemory);
     }
+
+    memset(t->src_addr, 0, size);
 
     if (R_SUCCEEDED(rc)) {
         rc = svcCreateTransferMemory(&t->handle, t->src_addr, size, perm);
