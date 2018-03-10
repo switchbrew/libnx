cb6cfca6c71e97faa78e0ea917164dbf19e4b413
plutoo <plutoo@universe>
2018-03-10 15:36:46 +0100

    gpu takes reference on singletons

--- nx/include/switch/result.h
+++ nx/include/switch/result.h
@@ -74,6 +74,8 @@ enum {
     LibnxError_TooManyDevOpTabs,
     LibnxError_AppletFailedToInitialize,
     LibnxError_ApmFailedToInitialize,
+    LibnxError_NvinfoFailedToInitialize,
+    LibnxError_NvbufFailedToInitialize,
 };
 
 /// libnx nvidia error codes
