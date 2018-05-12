a6ba19834d4ae19e8b11dd4df67c78509e3939d8
mdbell <contact@mdbell.me>
2018-05-12 17:30:25 -0400

    Added pm:info and GetTitleId

--- nx/include/switch/services/pm.h
+++ nx/include/switch/services/pm.h
@@ -3,6 +3,7 @@
  * @brief Process management (pm*) service IPC wrapper.
  * @author plutoo
  * @author yellows8
+ * @author mdbell
  * @copyright libnx Authors
  */
 #pragma once
@@ -10,6 +11,9 @@
 
 Result pmdmntInitialize(void);
 void pmdmntExit(void);
+
+Result pminfoInitialize(void);
+void pminfoExit(void);
 
 Result pmshellInitialize(void);
 void pmshellExit(void);
@@ -19,5 +23,7 @@ Result pmdmntGetTitlePid(u64* pid_out, u64 title_id);
 Result pmdmntEnableDebugForTitleId(Handle* handle_out, u64 title_id);
 Result pmdmntGetApplicationPid(u64* pid_out);
 Result pmdmntEnableDebugForApplication(Handle* handle_out);
+
+Result pminfoGetTitleId(u64* title_id_out, u64 pid);
 
 Result pmshellLaunchProcess(u32 launch_flags, u64 titleID, u64 storageID, u64 *pid);
