bf9001026399743041ba2fa4c50292a1321f0a84
Joel16 <sauskethunderkid@gmail.com>
2018-06-05 21:13:21 -0500

    Remove 'or 5'

--- nx/include/switch/services/ns.h
+++ nx/include/switch/services/ns.h
@@ -21,14 +21,14 @@ Result nsGetApplicationControlData(u8 flag, u64 titleID, NsApplicationControlDat
 
 /**
  * @brief Returns the total storage capacity (used + free) from content manager services.
- * @param storage_id Specified FsStorageId. (Must be FsStorageId_SdCard or 5)
+ * @param storage_id Specified FsStorageId. (Must be FsStorageId_SdCard)
  * @param size Pointer to output the total storage size to.
  */
 Result nsGetTotalSpaceSize(FsStorageId storage_id, u64 *size);
 
 /**
  * @brief Returns the available storage capacity from content manager services.
- * @param storage_id Specified FsStorageId. (Must be FsStorageId_SdCard or 5)
+ * @param storage_id Specified FsStorageId. (Must be FsStorageId_SdCard)
  * @param size Pointer to output the free storage size to.
  */
 Result nsGetFreeSpaceSize(FsStorageId storage_id, u64 *size);
