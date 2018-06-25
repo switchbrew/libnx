c26e410ed8931135295f0322c76bea9c7a3b16fc
Michael Scire <SciresM@gmail.com>
2018-06-24 22:34:39 -0600

    Fatal: Make fatalSimple not create report, add FatalType enum

--- nx/include/switch/services/fatal.h
+++ nx/include/switch/services/fatal.h
@@ -7,9 +7,23 @@
 #pragma once
 #include "../types.h"
 
+typedef enum {
+    FatalType_ErrorReportAndErrorScreen = 0,
+    FatalType_ErrorReport = 1,
+    FatalType_ErrorScreen = 2
+} FatalType;
+
 /**
  * @brief Triggers a system fatal error.
  * @param err[in] Result code to throw.
  * @note This function does not return.
  */
 void NORETURN fatalSimple(Result err);
+
+/**
+ * @brief Triggers a system fatal error with a custom FatalType.
+ * @param err[in] Result code to throw.
+ * @param err[in] Type of fatal error to throw.
+ * @note This function does not return.
+ */
+void NORETURN fatalWithType(Result err, FatalType type);
