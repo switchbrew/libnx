d29f44cdb1982e42792be55246101e64afd14e42
tesnos <kbmiller05@gmail.com>
2018-07-09 21:14:23 -0400

    newline

--- nx/include/switch/services/fs.h
+++ nx/include/switch/services/fs.h
@@ -144,6 +144,7 @@ void fsExit(void);
 
 Service* fsGetServiceSession(void);
 
+
 Result fsOpenBisStorage(FsStorage* out, u32 PartitionId);
 /// Do not call this directly, see fs_dev.h.
 Result fsMountSdcard(FsFileSystem* out);
