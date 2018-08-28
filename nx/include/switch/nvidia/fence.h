10684e205a4e7780d97a7d2ebe7f0f1582baa0de
fincs <fincs.alt1@gmail.com>
2018-08-29 00:51:24 +0200

    nvidia/fence.h: sketching on NvFence/NvMultiFence {not implemented}

--- nx/include/switch/nvidia/fence.h
+++ nx/include/switch/nvidia/fence.h
@@ -1,9 +1,18 @@
 #pragma once
+#include "ioctl.h"
+
+typedef nvioctl_fence NvFence;
 
 typedef struct {
-    nvioctl_fence fence;
-} NvFence;
+    u32 num_fences;
+    NvFence fences[4];
+} NvMultiFence;
 
-static inline void nvfenceCreate(NvFence* f, nvioctl_fence* fence) {
-    f->fence = *fence;
+Result nvFenceWait(NvFence* f, s32 timeout_ms);
+
+static inline void nvMultiFenceCreate(NvMultiFence* mf, const NvFence* fence) {
+    mf->num_fences = 1;
+    mf->fences[0] = *fence;
 }
+
+Result nvMultiFenceWait(NvMultiFence* mf, s32 timeout_ms);
