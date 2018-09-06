fc3145ff6ef886a2a0bb65db211fdb9315ce100c
XorTroll <33005497+XorTroll@users.noreply.github.com>
2018-09-07 00:09:03 +0200

    Add psm to main libnx header

--- nx/include/switch.h
+++ nx/include/switch.h
@@ -46,6 +46,7 @@ extern "C" {
 #include "switch/services/audout.h"
 #include "switch/services/csrng.h"
 #include "switch/services/bpc.h"
+#include "switch/services/psm.h"
 //#include "switch/services/bsd.h" Use switch/runtime/devices/socket.h instead
 #include "switch/services/fatal.h"
 #include "switch/services/time.h"
