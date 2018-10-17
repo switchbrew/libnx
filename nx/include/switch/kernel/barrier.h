5f6b8403605f89e187d4eb6472d1871baee34602
Tate Haga <tatehaga@gmail.com>
2018-10-17 18:52:16 -0400

    documentation syntax fixes

--- nx/include/switch/kernel/barrier.h
+++ nx/include/switch/kernel/barrier.h
@@ -7,13 +7,13 @@
 #pragma once
 #include "semaphore.h"
 
-/// Barrier structure
+/// Barrier structure.
 typedef struct Barrier {
-    u64 count;             ///< Number of threads to reach the barrier
-    u64 thread_total;      ///< Number of threads to wait on
-    Semaphore throttle;    ///< Semaphore to make sure threads release to scheduler one at a time
-    Semaphore lock;        ///< Semaphore to lock barrier to prevent multiple operations by threads at once
-    Semaphore thread_wait; ///< Semaphore to force a thread to wait if count < thread_total
+    u64 count;             ///< Number of threads to reach the barrier.
+    u64 thread_total;      ///< Number of threads to wait on.
+    Semaphore throttle;    ///< Semaphore to make sure threads release to scheduler one at a time.
+    Semaphore lock;        ///< Semaphore to lock barrier to prevent multiple operations by threads at once.
+    Semaphore thread_wait; ///< Semaphore to force a thread to wait if count < thread_total.
 } Barrier;
 
 /**
