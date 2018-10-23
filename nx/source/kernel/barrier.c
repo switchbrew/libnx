83e8602a91e742de1eed87113d4802a8cc344dff
plutooo <plutooo@users.noreply.github.com>
2018-10-23 23:02:23 +0200

    barrier: More efficient impl, maybe

--- nx/source/kernel/barrier.c
+++ nx/source/kernel/barrier.c
@@ -1,29 +1,22 @@
 #include "kernel/barrier.h"
 
-void barrierInit(Barrier *b, u64 thread_count) {
+void barrierInit(Barrier *b, u64 total) {
     b->count = 0;
-    b->thread_total = thread_count;
-    semaphoreInit(&b->throttle, 0);
-    semaphoreInit(&b->lock, 1);
-    semaphoreInit(&b->thread_wait, 0);
+    b->total = total - 1;
+    mutexInit(&b->mutex);
+    condvarInit(&b->condvar);
 }
 
 void barrierWait(Barrier *b) {
-    semaphoreWait(&b->lock);
-    if(b->count < b->thread_total) {
-        b->count++;
+    mutexLock(&b->mutex);
+
+    if (b->count++ == b->total) {
+      b->count = 0;
+      condvarWake(&b->condvar, b->total);
     }
-    if(b->count < b->thread_total) {
-        semaphoreSignal(&b->lock);
-        semaphoreWait(&b->thread_wait);
-        semaphoreSignal(&b->throttle);
+    else {
+       condvarWait(&b->condvar, &b->mutex);
     }
-    else if(b->count == b->thread_total) {
-        for(int i = 0; i < b->thread_total-1; i++) {
-            semaphoreSignal(&b->thread_wait);
-            semaphoreWait(&b->throttle);
-        }
-        b->count = 0;
-        semaphoreSignal(&b->lock);
-    }
+
+    mutexUnlock(&b->mutex);
 }
