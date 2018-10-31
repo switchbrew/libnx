f0de0c869da3c1e7435c2808db6a24658a16563d
fincs <fincs.alt1@gmail.com>
2018-10-31 15:06:08 +0100

    Added nvInfoGetGpuCharacteristics

--- nx/include/switch/nvidia/info.h
+++ nx/include/switch/nvidia/info.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "types.h"
+#include "ioctl.h"
 
 Result nvInfoInit(void);
 void nvInfoExit(void);
 
+const nvioctl_gpu_characteristics* nvInfoGetGpuCharacteristics(void);
 u32 nvInfoGetZcullCtxSize(void);
