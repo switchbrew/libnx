d87f9541d10b3fe0b9d494054b5c6ed478bb88b6
fincs <fincs.alt1@gmail.com>
2018-12-09 16:21:12 +0100

    NvMap: added support for loading remote NvMap objects (from id)

--- nx/include/switch/nvidia/map.h
+++ nx/include/switch/nvidia/map.h
@@ -16,6 +16,7 @@ u32    nvMapGetFd(void);
 void   nvMapExit(void);
 
 Result nvMapCreate(NvMap* m, void* cpu_addr, u32 size, u32 align, NvKind kind, bool is_cpu_cacheable);
+Result nvMapLoadRemote(NvMap* m, u32 id);
 void   nvMapClose(NvMap* m);
 
 static inline u32 nvMapGetHandle(NvMap* m)
@@ -28,7 +29,22 @@ static inline u32 nvMapGetId(NvMap* m)
     return m->id;
 }
 
+static inline u32 nvMapGetSize(NvMap* m)
+{
+    return m->size;
+}
+
 static inline void* nvMapGetCpuAddr(NvMap* m)
 {
     return m->cpu_addr;
+}
+
+static inline bool nvMapIsRemote(NvMap* m)
+{
+    return !m->cpu_addr;
+}
+
+static inline NvKind nvMapGetKind(NvMap* m)
+{
+    return m->kind;
 }
