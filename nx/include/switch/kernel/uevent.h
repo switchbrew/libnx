31d341804ff0a2cb429c09f7d9db84104ebb25cb
plutooo <plutooo@users.noreply.github.com>
2018-12-13 00:48:51 +0100

    wait: Doxygen

--- nx/include/switch/kernel/uevent.h
+++ nx/include/switch/kernel/uevent.h
@@ -1,6 +1,5 @@
 // Copyright 2018 plutoo
 #pragma once
-#include "../kernel/mutex.h"
 #include "../kernel/wait.h"
 
 typedef struct UsermodeEvent UsermodeEvent;
@@ -12,6 +11,21 @@ struct UsermodeEvent
     bool auto_clear;
 };
 
+/**
+ * @brief Creates a usermode event.
+ * @param[out] e UsermodeEvent object.
+ * @param[in] bool auto_clear Whether to automatically clear the event.
+ * @note It is safe to wait on this event with several threads simultaneously.
+ * @note If more than one thread is listening on it, at least one thread will get the signal. No other guarantees.
+ */
 void ueventCreate(UsermodeEvent* e, bool auto_clear);
+/**
+ * @brief Clears the event signal.
+ * @param[in] e UsermodeEvent object.
+ */
 void ueventClear(UsermodeEvent* e);
+/**
+ * @brief Signals the event.
+ * @param[in] e UsermodeEvent object.
+ */
 void ueventSignal(UsermodeEvent* e);
--- nx/include/switch/kernel/utimer.h
+++ nx/include/switch/kernel/utimer.h
@@ -1,6 +1,5 @@
 // Copyright 2018 plutoo
 #pragma once
-#include "../kernel/mutex.h"
 #include "../kernel/wait.h"
 
 typedef struct UsermodeTimer UsermodeTimer;
@@ -12,6 +11,23 @@ struct UsermodeTimer
     u64 interval;
 };
 
+/**
+ * @brief Creates a usermode timer.
+ * @param[out] t UsermodeTimer object.
+ * @param[in] interval Interval (in nanoseconds).
+ * @param[in] start Whether to start the timer right away.
+ * @note It is safe to wait on this timer with several threads simultaneously.
+ * @note If more than one thread is listening on it, at least one thread will get the signal. No other guarantees.
+ * @note If the timer triggers twice before you wait on it, you will only get one signal.
+ */
 void utimerCreate(UsermodeTimer* t, u64 interval, bool start);
+/**
+ * @brief Starts the timer.
+ * @param[in] t UsermodeTimer object.
+ */
 void utimerStart(UsermodeTimer* t);
+/**
+ * @brief Stops the timer.
+ * @param[in] t UsermodeTimer object.
+ */
 void utimerStop(UsermodeTimer* t);
