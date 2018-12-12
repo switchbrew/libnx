f2d60fb96424066086d9382a0caaf0d8bfadbe6a
plutooo <plutooo@users.noreply.github.com>
2018-12-13 00:29:27 +0100

    Revert "detect: Detect firmware during lib init, inline version checks"
    This reverts commit fa9f93342f74ced9cced9036e7ddd99bc688ff6e.

--- nx/include/switch/kernel/detect.h
+++ nx/include/switch/kernel/detect.h
@@ -7,37 +7,15 @@
 #pragma once
 #include "../types.h"
 
-extern bool g_IsAbove200;
-extern bool g_IsAbove300;
-extern bool g_IsAbove400;
-extern bool g_IsAbove500;
-extern bool g_IsAbove600;
-
 /// Returns true if the kernel version is equal to or above 2.0.0.
-static inline bool kernelAbove200(void) {
-    return g_IsAbove200;
-}
-
+bool kernelAbove200(void);
 /// Returns true if the kernel version is equal to or above 3.0.0.
-static inline bool kernelAbove300(void) {
-    return g_IsAbove300;
-}
-
+bool kernelAbove300(void);
 /// Returns true if the kernel version is equal to or above 4.0.0.
-static inline bool kernelAbove400(void) {
-    return g_IsAbove400;
-}
-
+bool kernelAbove400(void);
 /// Returns true if the kernel version is equal to or above 5.0.0.
-static inline bool kernelAbove500(void) {
-    return g_IsAbove500;
-}
-
+bool kernelAbove500(void);
 /// Returns true if the kernel version is equal to or above 6.0.0.
-static inline bool kernelAbove600(void) {
-    return g_IsAbove600;
-}
-
+bool kernelAbove600(void);
 /// Returns true if the process has a debugger attached.
 bool detectDebugger(void);
-
