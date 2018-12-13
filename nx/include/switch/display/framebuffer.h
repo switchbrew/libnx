0bc2356752e1924b067c9e2a0253d7624b7d28e0
fincs <fincs.alt1@gmail.com>
2018-12-13 23:56:41 +0100

    Add framebufferMakeLinear - linear shadow buffer support

--- nx/include/switch/display/framebuffer.h
+++ nx/include/switch/display/framebuffer.h
@@ -13,6 +13,7 @@ typedef struct Framebuffer {
     NWindow *win;
     NvMap map;
     void* buf;
+    void* buf_linear;
     u32 stride;
     u32 width_aligned;
     u32 height_aligned;
@@ -29,6 +30,8 @@ typedef struct Framebuffer {
  * @param[in] height Desired height of the framebuffer (usually 720).
  * @param[in] format Desired pixel format (see PIXEL_FORMAT_* enum).
  * @param[in] num_fbs Number of buffers to create. Pass 1 for single-buffering, 2 for double-buffering or 3 for triple-buffering.
+ * @note Framebuffer images are stored in Tegra block linear format with 16Bx2 sector ordering, read TRM chapter 20.1 for more details.
+ *       In order to use regular linear layout, consider calling \ref framebufferMakeLinear after the \ref Framebuffer object is created.
  * @note Presently, only the following pixel formats are supported:
  *       \ref PIXEL_FORMAT_RGBA_8888
  *       \ref PIXEL_FORMAT_RGBX_8888
@@ -38,24 +41,31 @@ typedef struct Framebuffer {
  */
 Result framebufferCreate(Framebuffer* fb, NWindow *win, u32 width, u32 height, u32 format, u32 num_fbs);
 
+/// Enables linear framebuffer mode in a \ref Framebuffer, allocating a shadow buffer in the process.
+Result framebufferMakeLinear(Framebuffer* fb);
+
 /// Closes a \ref Framebuffer object, freeing all resources associated with it.
 void framebufferClose(Framebuffer* fb);
 
 /**
- * @brief Begins rendering a frame in a Framebuffer.
+ * @brief Begins rendering a frame in a \ref Framebuffer.
  * @param[in] fb Pointer to \ref Framebuffer structure.
  * @param[out] out_stride Output variable containing the distance in bytes between rows of pixels in memory.
  * @return Pointer to buffer to which new graphics data should be written to.
  * @note When this function is called, a buffer will be dequeued from the corresponding \ref NWindow.
  * @note This function will return pointers to different buffers, depending on the number of buffers it was initialized with.
+ * @note If \ref framebufferMakeLinear was used, this function will instead return a pointer to the shadow linear buffer.
+ *       In this case, the offset of a pixel is \p y * \p out_stride + \p x * \p bytes_per_pixel.
  * @note Each call to \ref framebufferBegin must be paired with a \ref framebufferEnd call.
  */
 void* framebufferBegin(Framebuffer* fb, u32* out_stride);
 
 /**
- * @brief Finishes rendering a frame in a Framebuffer.
+ * @brief Finishes rendering a frame in a \ref Framebuffer.
  * @param[in] fb Pointer to \ref Framebuffer structure.
  * @note When this function is called, the written image data will be flushed and queued (presented) in the corresponding \ref NWindow.
+ * @note If \ref framebufferMakeLinear was used, this function will copy the image from the shadow linear buffer to the actual framebuffer,
+ *       converting it in the process to the layout expected by the compositor.
  * @note Each call to \ref framebufferBegin must be paired with a \ref framebufferEnd call.
  */
 void framebufferEnd(Framebuffer* fb);
