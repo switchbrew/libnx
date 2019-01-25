3702d001c3ec4f0ac6fcf1cc61621ed4a8e11c57
shibbo <xarf98@yahoo.com>
2019-01-25 12:54:47 -0500

    whitespace fix

--- nx/source/services/nifm.c
+++ nx/source/services/nifm.c
@@ -1,7 +1,7 @@
 /**
  * @file nifm.c
  * @brief Network interface service IPC wrapper.
- * @author shadowninja108, shibboleet 
+ * @author shadowninja108, shibboleet
  * @copyright libnx Authors
  */
 
