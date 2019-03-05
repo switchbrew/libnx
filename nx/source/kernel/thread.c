626b07fac8d13df801751164a3b1057db92d5e2b
fincs <fincs@devkitpro.org>
2019-03-05 16:37:34 +0100

    Implement TLS slot support (with destructors)

--- nx/source/kernel/thread.c
+++ nx/source/kernel/thread.c
@@ -5,14 +5,24 @@
 #include "result.h"
 #include "kernel/svc.h"
 #include "kernel/virtmem.h"
+#include "kernel/mutex.h"
 #include "kernel/thread.h"
 #include "kernel/wait.h"
+#include "services/fatal.h"
 #include "../internal.h"
+
+#define NUM_TLS_SLOTS ((0x100 - sizeof(ThreadVars)) / sizeof(void*))
 
 extern const u8 __tdata_lma[];
 extern const u8 __tdata_lma_end[];
 extern u8 __tls_start[];
 extern u8 __tls_end[];
+
+static Mutex g_threadMutex;
+static Thread* g_threadList;
+
+static u64 g_tlsUsageMask;
+static void (* g_tlsDestructors[NUM_TLS_SLOTS])(void*);
 
 // Thread creation args; keep this struct's size 16-byte aligned
 typedef struct {
@@ -33,9 +43,19 @@ static void _EntryWrap(ThreadEntryArgs* args) {
     tv->tls_tp     = (u8*)args->tls-2*sizeof(void*); // subtract size of Thread Control Block (TCB)
     tv->handle     = args->t->handle;
 
+    // Initialize thread info
+    mutexLock(&g_threadMutex);
+    args->t->tls_array = (void**)((u8*)armGetTls() + 0x100);
+    args->t->prev_next = &g_threadList;
+    args->t->next = g_threadList;
+    if (g_threadList)
+        g_threadList->prev_next = &args->t->next;
+    g_threadList = args->t;
+    mutexUnlock(&g_threadMutex);
+
     // Launch thread entrypoint
     args->entry(args->arg);
-    svcExitThread();
+    threadExit();
 }
 
 Result threadCreate(
@@ -72,6 +92,9 @@ Result threadCreate(
                 t->stack_mem = stack;
                 t->stack_mirror = stack_mirror;
                 t->stack_sz = stack_sz;
+                t->tls_array = NULL;
+                t->next = NULL;
+                t->prev_next = NULL;
 
                 args->t = t;
                 args->entry = entry;
@@ -109,6 +132,34 @@ Result threadCreate(
     return rc;
 }
 
+void threadExit(void) {
+    Thread* t = getThreadVars()->thread_ptr;
+    if (!t)
+        fatalSimple(MAKERESULT(Module_Libnx, LibnxError_NotInitialized));
+
+    u64 tls_mask = __atomic_load_n(&g_tlsUsageMask, __ATOMIC_SEQ_CST);
+    for (s32 i = 0; i < NUM_TLS_SLOTS; i ++) {
+        if (!(tls_mask & ((UINT64_C(1) << i))))
+            continue;
+        if (t->tls_array[i]) {
+            if (g_tlsDestructors[i])
+                g_tlsDestructors[i](t->tls_array[i]);
+            t->tls_array[i] = NULL;
+        }
+    }
+
+    mutexLock(&g_threadMutex);
+    *t->prev_next = t->next;
+    if (t->next)
+        t->next->prev_next = t->prev_next;
+    t->tls_array = NULL;
+    t->next = NULL;
+    t->prev_next = NULL;
+    mutexUnlock(&g_threadMutex);
+
+    svcExitThread();
+}
+
 Result threadStart(Thread* t) {
     return svcStartThread(t->handle);
 }
@@ -119,6 +170,9 @@ Result threadWaitForExit(Thread* t) {
 
 Result threadClose(Thread* t) {
     Result rc;
+
+    if (t->tls_array)
+        return MAKERESULT(Module_Libnx, LibnxError_BadInput);
 
     rc = svcUnmapMemory(t->stack_mirror, t->stack_mem, t->stack_sz);
     virtmemFreeStack(t->stack_mirror, t->stack_sz);
@@ -142,4 +196,44 @@ Result threadDumpContext(ThreadContext* ctx, Thread* t) {
 
 Handle threadGetCurHandle(void) {
     return getThreadVars()->handle;
+}
+
+s32 threadTlsAlloc(void (* destructor)(void*)) {
+    s32 slot_id;
+    u64 new_mask;
+    u64 cur_mask = __atomic_load_n(&g_tlsUsageMask, __ATOMIC_SEQ_CST);
+    do {
+        slot_id = __builtin_ffs(~cur_mask)-1;
+        if (slot_id < 0 || slot_id >= NUM_TLS_SLOTS) return -1;
+        new_mask = cur_mask | (UINT64_C(1) << slot_id);
+    } while (!__atomic_compare_exchange_n(&g_tlsUsageMask, &cur_mask, new_mask, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST));
+
+    threadTlsSet(slot_id, NULL);
+    mutexLock(&g_threadMutex);
+    for (Thread *t = g_threadList; t; t = t->next)
+        t->tls_array[slot_id] = NULL;
+    mutexUnlock(&g_threadMutex);
+
+    g_tlsDestructors[slot_id] = destructor;
+    return slot_id;
+}
+
+void* threadTlsGet(s32 slot_id) {
+    void** tls_array = (void**)((u8*)armGetTls() + 0x100);
+    return tls_array[slot_id];
+}
+
+void threadTlsSet(s32 slot_id, void* value) {
+    void** tls_array = (void**)((u8*)armGetTls() + 0x100);
+    tls_array[slot_id] = value;
+}
+
+void threadTlsFree(s32 slot_id) {
+    g_tlsDestructors[slot_id] = NULL;
+
+    u64 new_mask;
+    u64 cur_mask = __atomic_load_n(&g_tlsUsageMask, __ATOMIC_SEQ_CST);
+    do
+        new_mask = cur_mask &~ (UINT64_C(1) << slot_id);
+    while (!__atomic_compare_exchange_n(&g_tlsUsageMask, &cur_mask, new_mask, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST));
 }
