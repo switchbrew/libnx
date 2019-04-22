77097616714d0c6a661c8988af70710583d1085c
Michael Scire <SciresM@gmail.com>
2019-04-22 05:08:12 -0700

    Implement ro:1

--- nx/include/switch/services/ro.h
+++ nx/include/switch/services/ro.h
@@ -12,6 +12,9 @@
 Result ldrRoInitialize(void);
 void ldrRoExit(void);
 
+Result ro1Initialize(void);
+void ro1Exit(void);
+
 Result roDmntInitialize(void);
 void roDmntExit(void);
 
@@ -20,5 +23,11 @@ Result ldrRoUnloadNro(u64 nro_address);
 Result ldrRoLoadNrr(u64 nrr_address, u64 nrr_size);
 Result ldrRoUnloadNrr(u64 nrr_address);
 Result ldrRoLoadNrrEx(u64 nrr_address, u64 nrr_size);
+
+Result ro1LoadNro(u64* out_address, u64 nro_address, u64 nro_size, u64 bss_address, u64 bss_size);
+Result ro1UnloadNro(u64 nro_address);
+Result ro1LoadNrr(u64 nrr_address, u64 nrr_size);
+Result ro1UnloadNrr(u64 nrr_address);
+Result ro1LoadNrrEx(u64 nrr_address, u64 nrr_size);
 
 Result roDmntGetModuleInfos(u64 pid, LoaderModuleInfo *out_module_infos, size_t max_out_modules, u32 *num_out);
