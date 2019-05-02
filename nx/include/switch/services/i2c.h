4e16cea82eed74a0f613b3e2cde1cae5d989686e
Michael Scire <SciresM@gmail.com>
2019-05-02 02:49:29 -0700

    i2c: flesh out I2cDevice enum

--- nx/include/switch/services/i2c.h
+++ nx/include/switch/services/i2c.h
@@ -9,7 +9,37 @@
 #include "sm.h"
 
 typedef enum {
-    I2cDevice_AudioCodec = 4,
+    I2cDevice_DebugPad = 0,
+    I2cDevice_TouchPanel = 1,
+    I2cDevice_Tmp451 = 2,
+    I2cDevice_Nct72 = 3,
+    I2cDevice_Alc5639 = 4,
+    I2cDevice_Max77620Rtc = 5,
+    I2cDevice_Max77620Pmic = 6,
+    I2cDevice_Max77621Cpu = 7,
+    I2cDevice_Max77621Gpu = 8,
+    I2cDevice_Bq24193 = 9,
+    I2cDevice_Max17050 = 10,
+    I2cDevice_Bm92t30mwv = 11,
+    I2cDevice_Ina226Vdd15v0Hb = 12,
+    I2cDevice_Ina226VsysCpuDs = 13,
+    I2cDevice_Ina226VsysGpuDs = 14,
+    I2cDevice_Ina226VsysDdrDs = 15,
+    I2cDevice_Ina226VsysAp = 16,
+    I2cDevice_Ina226VsysBlDs = 17,
+    I2cDevice_Bh1730 = 18,
+    I2cDevice_Ina226VsysCore = 19,
+    I2cDevice_Ina226Soc1V8 = 20,
+    I2cDevice_Ina226Lpddr1V8 = 21,
+    I2cDevice_Ina226Reg1V32 = 22,
+    I2cDevice_Ina226Vdd3V3Sys = 23,
+    I2cDevice_HdmiDdc = 24,
+    I2cDevice_HdmiScdc = 25,
+    I2cDevice_HdmiHdcp = 26,
+    I2cDevice_Fan53528 = 27,
+    I2cDevice_Max77812_3 = 28,
+    I2cDevice_Max77812_2 = 29,
+    I2cDevice_Ina226VddDdr0V6 = 30,
 } I2cDevice;
 
 typedef struct {
