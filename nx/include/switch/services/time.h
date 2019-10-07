040b33c4573fefca24062a5112aba26111941074
yellows8 <yellows8@users.noreply.github.com>
2019-10-06 22:06:22 -0400

    time: Updated for new-ipc. Renamed _timeGetClockSession to timeGetServiceSession_SystemClock, and added it to the .h. Added timeGetServiceSession_TimeZoneService. Fixed the order of cmds. Fixed param types for timeGetTotalLocationNameCount, timeLoadLocationNameList, timeToPosixTime, and timeToPosixTimeWithMyRule. The location_name_size param for timeLoadLocationNameList was replaced with location_name_max, which is max entries instead of buffer byte-size. Internal improvements and improved docs.

--- nx/include/switch/services/time.h
+++ nx/include/switch/services/time.h
@@ -7,7 +7,7 @@
 #pragma once
 
 #include "../types.h"
-#include "../services/sm.h"
+#include "../sf/service.h"
 
 /// Time clock type.
 typedef enum {
@@ -28,11 +28,11 @@ typedef struct {
 } TimeCalendarTime;
 
 typedef struct {
-    u32 wday; ///< 0-based day-of-week.
-    u32 yday; ///< 0-based day-of-year.
-    char timezoneName[8]; ///< Timezone name string.
-    u32 DST; ///< 0 = no DST, 1 = DST.
-    s32 offset; ///< Seconds relative to UTC for this timezone.
+    u32 wday;              ///< 0-based day-of-week.
+    u32 yday;              ///< 0-based day-of-year.
+    char timezoneName[8];  ///< Timezone name string.
+    u32 DST;               ///< 0 = no DST, 1 = DST.
+    s32 offset;            ///< Seconds relative to UTC for this timezone.
 } TimeCalendarAdditionalInfo;
 
 typedef struct {
@@ -43,11 +43,27 @@ typedef struct {
     char name[0x24];
 } TimeLocationName;
 
+/// Initialize time. Used automatically during app startup.
 Result timeInitialize(void);
+
+/// Exit time. Used automatically during app startup.
 void timeExit(void);
 
+/// Gets the Service object for the actual time service session.
 Service* timeGetServiceSession(void);
 
+/// Gets the Service object for ISystemClock with the specified \ref TimeType. This will return NULL when the type is invalid.
+Service* timeGetServiceSession_SystemClock(TimeType type);
+
+/// Gets the Service object for ITimeZoneService.
+Service* timeGetServiceSession_TimeZoneService(void);
+
+/**
+ * @brief Gets the time for the specified clock.
+ * @param[in] type Clock to use.
+ * @param[out] timestamp POSIX UTC timestamp.
+ * @return Result code.
+ */
 Result timeGetCurrentTime(TimeType type, u64 *timestamp);
 
 /**
@@ -60,13 +76,13 @@ Result timeSetCurrentTime(TimeType type, u64 timestamp);
 
 Result timeGetDeviceLocationName(TimeLocationName *name);
 Result timeSetDeviceLocationName(const TimeLocationName *name);
-Result timeGetTotalLocationNameCount(u32 *total_location_name_count);
-Result timeLoadLocationNameList(u32 index, TimeLocationName *location_name_array, size_t location_name_size, u32 *location_name_count);
+Result timeGetTotalLocationNameCount(s32 *total_location_name_count);
+Result timeLoadLocationNameList(s32 index, TimeLocationName *location_name_array, s32 location_name_max, s32 *location_name_count);
 
 Result timeLoadTimeZoneRule(const TimeLocationName *name, TimeZoneRule *rule);
 
-Result timeToPosixTime(const TimeZoneRule *rule, const TimeCalendarTime *caltime, u64 *timestamp_list, size_t timestamp_list_count, u32 *timestamp_count);
-Result timeToPosixTimeWithMyRule(const TimeCalendarTime *caltime, u64 *timestamp_list, size_t timestamp_list_count, u32 *timestamp_count);
 Result timeToCalendarTime(const TimeZoneRule *rule, u64 timestamp, TimeCalendarTime *caltime, TimeCalendarAdditionalInfo *info);
 Result timeToCalendarTimeWithMyRule(u64 timestamp, TimeCalendarTime *caltime, TimeCalendarAdditionalInfo *info);
+Result timeToPosixTime(const TimeZoneRule *rule, const TimeCalendarTime *caltime, u64 *timestamp_list, s32 timestamp_list_count, s32 *timestamp_count);
+Result timeToPosixTimeWithMyRule(const TimeCalendarTime *caltime, u64 *timestamp_list, s32 timestamp_list_count, s32 *timestamp_count);
 
