0499ae3688f57591a3e5ae388c7964e10b7f1468
Michael Scire <SciresM@gmail.com>
2019-12-03 00:15:29 -0800

    rwlock: revamp completely
    Implements rwlockTryReadLock and rwlockTryWriteLock.
    Also implements rwlockIsWriteLockHeldByCurrentThread and
    rwlockIsOwnedByCurrentThread.
    
    Also re-designs RwLock to have semantics identical to
    Nintendo's (nn::os::ReaderWriterLock). The upshot is mostly
    that the lock is now fully recursive/write-preferring.

--- nx/include/switch/kernel/rwlock.h
+++ nx/include/switch/kernel/rwlock.h
@@ -1,7 +1,7 @@
 /**
  * @file rwlock.h
  * @brief Read/write lock synchronization primitive.
- * @author plutoo
+ * @author plutoo, SciresM
  * @copyright libnx Authors
  */
 #pragma once
@@ -11,10 +11,13 @@
 /// Read/write lock structure.
 typedef struct {
     Mutex mutex;
-    CondVar condvar_readers;
-    CondVar condvar_writer;
-    u32  readers : 31;
-    bool writer : 1;
+    CondVar condvar_reader_wait;
+    CondVar condvar_writer_wait;
+    u32 read_lock_count;
+    u32 read_waiter_count;
+    u32 write_lock_count;
+    u32 write_waiter_count;
+    u32 write_owner_tag;
 } RwLock;
 
 /**
@@ -30,6 +33,13 @@ void rwlockInit(RwLock* r);
 void rwlockReadLock(RwLock* r);
 
 /**
+ * @brief Attempts to lock the read/write lock for reading without waiting.
+ * @param r Read/write lock object.
+ * @return 1 if the mutex has been acquired successfully, and 0 on contention.
+ */
+bool rwlockTryReadLock(RwLock* r);
+
+/**
  * @brief Unlocks the read/write lock for reading.
  * @param r Read/write lock object.
  */
@@ -42,7 +52,29 @@ void rwlockReadUnlock(RwLock* r);
 void rwlockWriteLock(RwLock* r);
 
 /**
+ * @brief Attempts to lock the read/write lock for writing without waiting.
+ * @param r Read/write lock object.
+ * @return 1 if the mutex has been acquired successfully, and 0 on contention.
+ */
+bool rwlockTryWriteLock(RwLock* r);
+
+/**
  * @brief Unlocks the read/write lock for writing.
  * @param r Read/write lock object.
  */
 void rwlockWriteUnlock(RwLock* r);
+
+/**
+ * @brief Checks if the write lock is held by the current thread.
+ * @param r Read/write lock object.
+ * @return 1 if the current hold holds the write lock, and 0 if it does not.
+ */
+bool rwlockIsWriteLockHeldByCurrentThread(RwLock* r);
+
+/**
+ * @brief Checks if the read/write lock is owned by the current thread.
+ * @param r Read/write lock object.
+ * @return 1 if the current hold holds the write lock or if it holds read locks acquired
+ *         while it held the write lock, and 0 if it does not.
+ */
+bool rwlockIsOwnedByCurrentThread(RwLock* r);
