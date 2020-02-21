fbdb73abd9a535d5c8cb4ae675027bad26507e08
HookedBehemoth <Luisscheurenbrand@gmail.com>
2020-02-21 13:49:58 +0100

    fan: add docs

--- nx/include/switch/services/fan.h
+++ nx/include/switch/services/fan.h
@@ -1,10 +1,23 @@
+/**
+ * @file fan.h
+ * @brief Fan service IPC wrapper.
+ * @author Behemoth
+ * @copyright libnx Authors
+ */
 #pragma once
 #include "../types.h"
 #include "../sf/service.h"
 
+/// Initialize fan.
 Result fanInitialize(void);
+
+/// Exit fan.
 void fanExit(void);
+
+/// Gets the Service object for the actual fan service session.
 Service* fanGetServiceSession(void);
+
+/// Gets the Service object for the actual fan controller service session.
 Service* fanGetServiceSession_Controller(void);
 
 Result fanSetRotationSpeedLevel(float level);
