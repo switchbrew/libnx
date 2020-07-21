04817e130b8c356c01e329180177c005f30bf07c
yellows8 <yellows8@users.noreply.github.com>
2020-07-21 11:52:14 -0400

    btdrv: Added HidReportEventInfo support.

--- nx/source/services/btdrv.c
+++ nx/source/services/btdrv.c
@@ -1,10 +1,17 @@
 #define NX_SERVICE_ASSUME_NON_DOMAIN
 #include <string.h>
+#include <stdatomic.h>
 #include "service_guard.h"
+#include "arm/counter.h"
+#include "kernel/shmem.h"
 #include "runtime/hosversion.h"
 #include "services/btdrv.h"
 
 static Service g_btdrvSrv;
+
+static bool g_btdrvSharedmemInitialized;
+static SharedMemory g_btdrvSharedmem;
+static BtdrvCircularBuffer *g_btdrvCircularBuffer;
 
 static Result _btdrvCmdNoIO(u32 cmd_id);
 
@@ -19,6 +26,8 @@ Result _btdrvInitialize(void) {
 }
 
 void _btdrvCleanup(void) {
+    g_btdrvCircularBuffer = NULL;
+    shmemClose(&g_btdrvSharedmem);
     serviceClose(&g_btdrvSrv);
 }
 
@@ -28,6 +37,29 @@ Service* btdrvGetServiceSession(void) {
 
 static Result _btdrvCmdNoIO(u32 cmd_id) {
     return serviceDispatch(&g_btdrvSrv, cmd_id);
+}
+
+static Result _btdrvCmdGetHandle(Handle* handle_out, u32 cmd_id) {
+    return serviceDispatch(&g_btdrvSrv, cmd_id,
+        .out_handle_attrs = { SfOutHandleAttr_HipcCopy },
+        .out_handles = handle_out,
+    );
+}
+
+static Result _btdrvCmdGetEvent(Event* out_event, bool autoclear, u32 cmd_id) {
+    Handle tmp_handle = INVALID_HANDLE;
+    Result rc = 0;
+
+    rc = _btdrvCmdGetHandle(&tmp_handle, cmd_id);
+    if (R_SUCCEEDED(rc)) eventLoadRemote(out_event, tmp_handle, autoclear);
+    return rc;
+}
+
+static Result _btdrvCmdOutU32OutBuf(void* buffer, size_t size, u32 *out, u32 cmd_id) {
+    return serviceDispatchOut(&g_btdrvSrv, cmd_id, *out,
+        .buffer_attrs = { SfBufferAttr_HipcPointer | SfBufferAttr_Out },
+        .buffers = { { buffer, size } },
+    );
 }
 
 Result btdrvGetAdapterProperties(BtdrvAdapterProperty *property) {
@@ -91,6 +123,84 @@ Result btdrvGetHidReport(BtdrvAddress addr, u8 unk, u32 type) {
     } in = { addr, unk, {0}, type };
 
     return serviceDispatchIn(&g_btdrvSrv, 22, in);
+}
+
+Result btdrvRegisterHidReportEvent(Event* out_event) {
+    Result rc=0;
+    Handle tmphandle=0;
+
+    if (hosversionAtLeast(7,0,0)) {
+        if (!g_btdrvSharedmemInitialized) {
+            rc = _btdrvCmdGetHandle(&tmphandle, 38); // GetHidReportEventInfo
+
+            if (R_SUCCEEDED(rc)) {
+                shmemLoadRemote(&g_btdrvSharedmem, tmphandle, 0x3000, Perm_Rw);
+
+                rc = shmemMap(&g_btdrvSharedmem);
+            }
+
+            if (R_SUCCEEDED(rc)) {
+                g_btdrvSharedmemInitialized = true;
+                g_btdrvCircularBuffer = shmemGetAddr(&g_btdrvSharedmem);
+            }
+            else shmemClose(&g_btdrvSharedmem);
+        }
+        else {
+            while (btdrvCircularBufferRead(g_btdrvCircularBuffer)) btdrvCircularBufferFree(g_btdrvCircularBuffer);
+        }
+    }
+
+    if (R_SUCCEEDED(rc)) rc = _btdrvCmdGetEvent(out_event, true, 37); // RegisterHidReportEvent
+    return rc;
+}
+
+Result btdrvGetHidReportEventInfo(void* buffer, size_t size, BtdrvHidEventType *type) {
+    if (hosversionAtLeast(7,0,0)) {
+        BtdrvHidReportEventInfo *info = (BtdrvHidReportEventInfo*)buffer;
+        BtdrvHidReportEventInfoBufferData *data_ptr = NULL;
+        memset(buffer, 0, size);
+        if (g_btdrvCircularBuffer==NULL) return MAKERESULT(Module_Libnx, LibnxError_NotInitialized);
+        for (; (data_ptr = btdrvCircularBufferRead(g_btdrvCircularBuffer)); btdrvCircularBufferFree(g_btdrvCircularBuffer)) {
+            *type = data_ptr->hdr.type;
+            if (*type == BtdrvHidEventType_Unknown4) {
+                if (armTicksToNs(armGetSystemTick() - data_ptr->hdr.tick) >= 100000001) continue;
+            }
+            break;
+        }
+        if (data_ptr == NULL) {
+            *type = BtdrvHidEventType_Unknown4;
+            return 0;
+        }
+        if (*type == BtdrvHidEventType_Unknown9) {
+            if (hosversionBefore(9,0,0)) memcpy(info->type9.hid_data.rawdata, data_ptr->data.type9.hid_data.rawdata, sizeof(info->type9.hid_data.rawdata));
+            else memcpy(info->type9.hid_report.rawdata, data_ptr->data.type9.hid_report.rawdata, sizeof(info->type9.hid_report.rawdata));
+        }
+        else if (*type == BtdrvHidEventType_Unknown8) memcpy(info->type8.data, data_ptr->data.type8.data, sizeof(info->type8.data));
+        else if (*type == BtdrvHidEventType_Unknown4) {
+            u16 tmpsize = hosversionBefore(9,0,0) ? data_ptr->data.type4.v1.size : data_ptr->data.type4.v9.size;
+            if (size < 0xE) return MAKERESULT(Module_Libnx, LibnxError_BadInput);
+            if (tmpsize > size-0xE) tmpsize = size-0xE;
+            info->type4.unk_x0 = 0;
+            info->type4.size = tmpsize;
+            if (hosversionBefore(9,0,0)) memcpy(info->type4.data, data_ptr->data.type4.v1.data, tmpsize);
+            else memcpy(info->type4.data, data_ptr->data.type4.v9.data, tmpsize);
+
+            if (hosversionBefore(9,0,0)) memcpy(&info->type4.addr, &data_ptr->data.type4.v1.addr, sizeof(BtdrvAddress));
+            else memcpy(&info->type4.addr, &data_ptr->data.type4.v9.addr, sizeof(BtdrvAddress));
+        }
+        else return MAKERESULT(Module_Libnx, LibnxError_ShouldNotHappen); // sdknso would Abort here.
+        btdrvCircularBufferFree(g_btdrvCircularBuffer);
+        return 0;
+    }
+
+    u32 tmp_type=0;
+    Result rc = _btdrvCmdOutU32OutBuf(buffer, size, &tmp_type, 38);
+    if (R_SUCCEEDED(rc) && type) *type = tmp_type;
+    return rc;
+}
+
+void* btdrvGetHidReportEventInfoSharedmemAddr(void) {
+    return g_btdrvCircularBuffer;
 }
 
 Result btdrvReadGattCharacteristic(bool flag, u8 unk, u32 unk2, const BtdrvGattId *id0, const BtdrvGattId *id1) {
@@ -202,10 +312,7 @@ Result btdrvGetLeEventInfo(void* buffer, size_t size, u32 *type) {
         return MAKERESULT(Module_Libnx, LibnxError_IncompatSysVer);
     u32 cmd_id = hosversionBefore(6,0,0) ? 95 : 96;
 
-    return serviceDispatchOut(&g_btdrvSrv, cmd_id, *type,
-        .buffer_attrs = { SfBufferAttr_HipcPointer | SfBufferAttr_Out },
-        .buffers = { { buffer, size } },
-    );
+    return _btdrvCmdOutU32OutBuf(buffer, size, type, cmd_id);
 }
 
 Result btdrvRegisterBleHidEvent(Event* out_event) {
@@ -213,14 +320,55 @@ Result btdrvRegisterBleHidEvent(Event* out_event) {
         return MAKERESULT(Module_Libnx, LibnxError_IncompatSysVer);
     u32 cmd_id = hosversionBefore(6,0,0) ? 96 : 97;
 
-    Handle tmp_handle = INVALID_HANDLE;
-    Result rc = 0;
+    return _btdrvCmdGetEvent(out_event, true, cmd_id);
+}
 
-    rc = serviceDispatch(&g_btdrvSrv, cmd_id,
-        .out_handle_attrs = { SfOutHandleAttr_HipcCopy },
-        .out_handles = &tmp_handle,
-    );
-    if (R_SUCCEEDED(rc)) eventLoadRemote(out_event, tmp_handle, true);
-    return rc;
+// CircularBuffer
+
+void* btdrvCircularBufferRead(BtdrvCircularBuffer *c) {
+    if (c==NULL || !c->initialized) return NULL;
+
+    do {
+        s32 read_offset = atomic_load_explicit(&c->read_offset, memory_order_acquire);
+        s32 write_offset = atomic_load_explicit(&c->write_offset, memory_order_acquire);
+        if (read_offset == write_offset) return NULL;
+
+        u8 *data_ptr = &c->data[read_offset];
+        if (read_offset >= sizeof(c->data)) return NULL;
+
+        if (*data_ptr != 0xFF) return data_ptr;
+        else {
+            if (!c->initialized) return NULL;
+            read_offset = atomic_load_explicit(&c->read_offset, memory_order_acquire);
+            write_offset = atomic_load_explicit(&c->write_offset, memory_order_acquire);
+            if (read_offset == write_offset) continue;
+
+            data_ptr = &c->data[read_offset];
+            u64 tmpsize = read_offset + 0x18;
+            if (tmpsize < sizeof(c->data)) tmpsize += *((u64*)&data_ptr[0x10]);
+            if (tmpsize >= sizeof(c->data)) tmpsize = 0;
+            atomic_store_explicit(&c->read_offset, tmpsize, memory_order_release);
+        }
+    } while (c->initialized);
+
+    return NULL;
+}
+
+bool btdrvCircularBufferFree(BtdrvCircularBuffer *c) {
+    if (c==NULL || !c->initialized) return false;
+
+    s32 read_offset = atomic_load_explicit(&c->read_offset, memory_order_acquire);
+    s32 write_offset = atomic_load_explicit(&c->write_offset, memory_order_acquire);
+    if (read_offset == write_offset) return false;
+
+    u8 *data_ptr = &c->data[read_offset];
+    if (read_offset >= sizeof(c->data)) false;
+
+    u64 tmpsize = read_offset + 0x18;
+    if (tmpsize < sizeof(c->data)) tmpsize += *((u64*)&data_ptr[0x10]);
+    if (tmpsize >= sizeof(c->data)) tmpsize = 0;
+    atomic_store_explicit(&c->read_offset, tmpsize, memory_order_release);
+
+    return true;
 }
 
