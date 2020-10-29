eecf2a9d32bf2c992ac1f1d6788237d3b06fa333
fincs <fincs@devkitpro.org>
2020-10-29 16:52:16 +0100

    Add detectMesosphere()

--- nx/include/switch/kernel/detect.h
+++ nx/include/switch/kernel/detect.h
@@ -6,6 +6,7 @@
  */
 #pragma once
 #include "../types.h"
+#include "../result.h"
 #include "svc.h"
 
 /// Returns true if the process has a debugger attached.
@@ -13,4 +14,11 @@ NX_INLINE bool detectDebugger(void) {
     u64 tmp = 0;
     Result rc = svcGetInfo(&tmp, InfoType_DebuggerAttached, INVALID_HANDLE, 0);
     return R_SUCCEEDED(rc) && tmp != 0;
+}
+
+/// Returns true if the underlying kernel is Mesosphère.
+NX_INLINE bool detectMesosphere(void) {
+    u64 dummy = 0;
+    Result rc = svcGetInfo(&dummy, 65000, INVALID_HANDLE, 0); // InfoType_MesosphereMeta
+    return R_SUCCEEDED(rc);
 }
