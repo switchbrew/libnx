e48f1c339ff22b3b1951f3dfd05b54b4ba5b075a
SciresM <Sciresm@gmail.com>
2021-01-27 14:20:41 -0800

    add hosversionIsAtmosphere (#525)

--- nx/source/runtime/hosversion.c
+++ nx/source/runtime/hosversion.c
@@ -4,10 +4,15 @@ static u32 g_hosVersion;
 
 u32 hosversionGet(void)
 {
-    return __atomic_load_n(&g_hosVersion, __ATOMIC_SEQ_CST);
+    return __atomic_load_n(&g_hosVersion, __ATOMIC_SEQ_CST) & ~(BIT(31));
 }
 
 void hosversionSet(u32 version)
 {
     __atomic_store_n(&g_hosVersion, version, __ATOMIC_SEQ_CST);
+}
+
+bool hosversionIsAtmosphere(void)
+{
+    return __atomic_load_n(&g_hosVersion, __ATOMIC_SEQ_CST) & (BIT(31));
 }
