dcd9d4eb41d43806b58157db69a3ea46a9d3075b
Michael Scire <SciresM@gmail.com>
2021-02-19 15:39:44 -0800

    htcs: implement hipc bindings

--- nx/include/switch.h
+++ nx/include/switch.h
@@ -71,6 +71,7 @@ extern "C" {
 #include "switch/services/spsm.h"
 //#include "switch/services/bsd.h" Use <sys/socket.h> instead
 //#include "switch/services/sfdnsres.h" Use <netdb.h> instead
+//#include "switch/services/htcs.h"
 #include "switch/services/fatal.h"
 #include "switch/services/time.h"
 #include "switch/services/usb.h"
