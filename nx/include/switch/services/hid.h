c3ea95c97d267e5387ff83eb25b134d9663dbfd4
fincs <fincs@devkitpro.org>
2021-06-12 16:35:35 +0200

    Revert "Delete old HID API 🦀🦀"
    This reverts commit 637dd12b2df58c469d4e87a48aa3ebf0bc359766.

--- nx/include/switch/services/hid.h
+++ nx/include/switch/services/hid.h
@@ -11,6 +11,9 @@
 #include "../services/btdrv_types.h"
 #include "../sf/service.h"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
 // Begin enums and output structs
 
 /// HidDebugPadButton
@@ -197,6 +200,184 @@ typedef enum {
     HidKeyboardModifier_Hiragana      = BIT(12),
 } HidKeyboardModifier;
 
+/// HidKeyboardScancode \deprecated
+typedef enum DEPRECATED {
+    KBD_NONE = 0x00,
+    KBD_ERR_OVF = 0x01,
+
+    KBD_A = 0x04,
+    KBD_B = 0x05,
+    KBD_C = 0x06,
+    KBD_D = 0x07,
+    KBD_E = 0x08,
+    KBD_F = 0x09,
+    KBD_G = 0x0a,
+    KBD_H = 0x0b,
+    KBD_I = 0x0c,
+    KBD_J = 0x0d,
+    KBD_K = 0x0e,
+    KBD_L = 0x0f,
+    KBD_M = 0x10,
+    KBD_N = 0x11,
+    KBD_O = 0x12,
+    KBD_P = 0x13,
+    KBD_Q = 0x14,
+    KBD_R = 0x15,
+    KBD_S = 0x16,
+    KBD_T = 0x17,
+    KBD_U = 0x18,
+    KBD_V = 0x19,
+    KBD_W = 0x1a,
+    KBD_X = 0x1b,
+    KBD_Y = 0x1c,
+    KBD_Z = 0x1d,
+
+    KBD_1 = 0x1e,
+    KBD_2 = 0x1f,
+    KBD_3 = 0x20,
+    KBD_4 = 0x21,
+    KBD_5 = 0x22,
+    KBD_6 = 0x23,
+    KBD_7 = 0x24,
+    KBD_8 = 0x25,
+    KBD_9 = 0x26,
+    KBD_0 = 0x27,
+
+    KBD_ENTER = 0x28,
+    KBD_ESC = 0x29,
+    KBD_BACKSPACE = 0x2a,
+    KBD_TAB = 0x2b,
+    KBD_SPACE = 0x2c,
+    KBD_MINUS = 0x2d,
+    KBD_EQUAL = 0x2e,
+    KBD_LEFTBRACE = 0x2f,
+    KBD_RIGHTBRACE = 0x30,
+    KBD_BACKSLASH = 0x31,
+    KBD_HASHTILDE = 0x32,
+    KBD_SEMICOLON = 0x33,
+    KBD_APOSTROPHE = 0x34,
+    KBD_GRAVE = 0x35,
+    KBD_COMMA = 0x36,
+    KBD_DOT = 0x37,
+    KBD_SLASH = 0x38,
+    KBD_CAPSLOCK = 0x39,
+
+    KBD_F1 = 0x3a,
+    KBD_F2 = 0x3b,
+    KBD_F3 = 0x3c,
+    KBD_F4 = 0x3d,
+    KBD_F5 = 0x3e,
+    KBD_F6 = 0x3f,
+    KBD_F7 = 0x40,
+    KBD_F8 = 0x41,
+    KBD_F9 = 0x42,
+    KBD_F10 = 0x43,
+    KBD_F11 = 0x44,
+    KBD_F12 = 0x45,
+
+    KBD_SYSRQ = 0x46,
+    KBD_SCROLLLOCK = 0x47,
+    KBD_PAUSE = 0x48,
+    KBD_INSERT = 0x49,
+    KBD_HOME = 0x4a,
+    KBD_PAGEUP = 0x4b,
+    KBD_DELETE = 0x4c,
+    KBD_END = 0x4d,
+    KBD_PAGEDOWN = 0x4e,
+    KBD_RIGHT = 0x4f,
+    KBD_LEFT = 0x50,
+    KBD_DOWN = 0x51,
+    KBD_UP = 0x52,
+
+    KBD_NUMLOCK = 0x53,
+    KBD_KPSLASH = 0x54,
+    KBD_KPASTERISK = 0x55,
+    KBD_KPMINUS = 0x56,
+    KBD_KPPLUS = 0x57,
+    KBD_KPENTER = 0x58,
+    KBD_KP1 = 0x59,
+    KBD_KP2 = 0x5a,
+    KBD_KP3 = 0x5b,
+    KBD_KP4 = 0x5c,
+    KBD_KP5 = 0x5d,
+    KBD_KP6 = 0x5e,
+    KBD_KP7 = 0x5f,
+    KBD_KP8 = 0x60,
+    KBD_KP9 = 0x61,
+    KBD_KP0 = 0x62,
+    KBD_KPDOT = 0x63,
+
+    KBD_102ND = 0x64,
+    KBD_COMPOSE = 0x65,
+    KBD_POWER = 0x66,
+    KBD_KPEQUAL = 0x67,
+
+    KBD_F13 = 0x68,
+    KBD_F14 = 0x69,
+    KBD_F15 = 0x6a,
+    KBD_F16 = 0x6b,
+    KBD_F17 = 0x6c,
+    KBD_F18 = 0x6d,
+    KBD_F19 = 0x6e,
+    KBD_F20 = 0x6f,
+    KBD_F21 = 0x70,
+    KBD_F22 = 0x71,
+    KBD_F23 = 0x72,
+    KBD_F24 = 0x73,
+
+    KBD_OPEN = 0x74,
+    KBD_HELP = 0x75,
+    KBD_PROPS = 0x76,
+    KBD_FRONT = 0x77,
+    KBD_STOP = 0x78,
+    KBD_AGAIN = 0x79,
+    KBD_UNDO = 0x7a,
+    KBD_CUT = 0x7b,
+    KBD_COPY = 0x7c,
+    KBD_PASTE = 0x7d,
+    KBD_FIND = 0x7e,
+    KBD_MUTE = 0x7f,
+    KBD_VOLUMEUP = 0x80,
+    KBD_VOLUMEDOWN = 0x81,
+    KBD_CAPSLOCK_ACTIVE = 0x82 ,
+    KBD_NUMLOCK_ACTIVE = 0x83 ,
+    KBD_SCROLLLOCK_ACTIVE = 0x84 ,
+    KBD_KPCOMMA = 0x85,
+
+    KBD_KPLEFTPAREN = 0xb6,
+    KBD_KPRIGHTPAREN = 0xb7,
+
+    KBD_LEFTCTRL = 0xe0,
+    KBD_LEFTSHIFT = 0xe1,
+    KBD_LEFTALT = 0xe2,
+    KBD_LEFTMETA = 0xe3,
+    KBD_RIGHTCTRL = 0xe4,
+    KBD_RIGHTSHIFT = 0xe5,
+    KBD_RIGHTALT = 0xe6,
+    KBD_RIGHTMETA = 0xe7,
+
+    KBD_MEDIA_PLAYPAUSE = 0xe8,
+    KBD_MEDIA_STOPCD = 0xe9,
+    KBD_MEDIA_PREVIOUSSONG = 0xea,
+    KBD_MEDIA_NEXTSONG = 0xeb,
+    KBD_MEDIA_EJECTCD = 0xec,
+    KBD_MEDIA_VOLUMEUP = 0xed,
+    KBD_MEDIA_VOLUMEDOWN = 0xee,
+    KBD_MEDIA_MUTE = 0xef,
+    KBD_MEDIA_WWW = 0xf0,
+    KBD_MEDIA_BACK = 0xf1,
+    KBD_MEDIA_FORWARD = 0xf2,
+    KBD_MEDIA_STOP = 0xf3,
+    KBD_MEDIA_FIND = 0xf4,
+    KBD_MEDIA_SCROLLUP = 0xf5,
+    KBD_MEDIA_SCROLLDOWN = 0xf6,
+    KBD_MEDIA_EDIT = 0xf7,
+    KBD_MEDIA_SLEEP = 0xf8,
+    KBD_MEDIA_COFFEE = 0xf9,
+    KBD_MEDIA_REFRESH = 0xfa,
+    KBD_MEDIA_CALC = 0xfb
+} HidKeyboardScancode;
+
 /// KeyboardLockKeyEvent
 typedef enum {
     HidKeyboardLockKeyEvent_NumLockOn         = BIT(0),         ///< NumLockOn
@@ -293,6 +474,63 @@ typedef enum {
     HidNpadButton_AnySR    = HidNpadButton_LeftSR | HidNpadButton_RightSR,                                 ///< Bitmask containing SR buttons on both Joy-Cons (Left/Right)
 } HidNpadButton;
 
+/// HidControllerKeys \deprecated
+typedef enum DEPRECATED {
+    KEY_A            = HidNpadButton_A,
+    KEY_B            = HidNpadButton_B,
+    KEY_X            = HidNpadButton_X,
+    KEY_Y            = HidNpadButton_Y,
+    KEY_LSTICK       = HidNpadButton_StickL,
+    KEY_RSTICK       = HidNpadButton_StickR,
+    KEY_L            = HidNpadButton_L,
+    KEY_R            = HidNpadButton_R,
+    KEY_ZL           = HidNpadButton_ZL,
+    KEY_ZR           = HidNpadButton_ZR,
+    KEY_PLUS         = HidNpadButton_Plus,
+    KEY_MINUS        = HidNpadButton_Minus,
+    KEY_DLEFT        = HidNpadButton_Left,
+    KEY_DUP          = HidNpadButton_Up,
+    KEY_DRIGHT       = HidNpadButton_Right,
+    KEY_DDOWN        = HidNpadButton_Down,
+    KEY_LSTICK_LEFT  = HidNpadButton_StickLLeft,
+    KEY_LSTICK_UP    = HidNpadButton_StickLUp,
+    KEY_LSTICK_RIGHT = HidNpadButton_StickLRight,
+    KEY_LSTICK_DOWN  = HidNpadButton_StickLDown,
+    KEY_RSTICK_LEFT  = HidNpadButton_StickRLeft,
+    KEY_RSTICK_UP    = HidNpadButton_StickRUp,
+    KEY_RSTICK_RIGHT = HidNpadButton_StickRRight,
+    KEY_RSTICK_DOWN  = HidNpadButton_StickRDown,
+    KEY_SL_LEFT      = HidNpadButton_LeftSL,
+    KEY_SR_LEFT      = HidNpadButton_LeftSR,
+    KEY_SL_RIGHT     = HidNpadButton_RightSL,
+    KEY_SR_RIGHT     = HidNpadButton_RightSR,
+    KEY_NES_HANDHELD_LEFT_B = HidNpadButton_HandheldLeftB,
+
+    KEY_HOME         = BIT(18),      ///< HOME button, only available for use with HiddbgHdlsState::buttons.
+    KEY_CAPTURE      = BIT(19),      ///< Capture button, only available for use with HiddbgHdlsState::buttons.
+    KEY_TOUCH        = BIT(28),      ///< Pseudo-key for at least one finger on the touch screen
+
+    KEY_JOYCON_RIGHT = HidNpadButton_A,
+    KEY_JOYCON_DOWN  = HidNpadButton_B,
+    KEY_JOYCON_UP    = HidNpadButton_X,
+    KEY_JOYCON_LEFT  = HidNpadButton_Y,
+
+    KEY_UP    = HidNpadButton_AnyUp,
+    KEY_DOWN  = HidNpadButton_AnyDown,
+    KEY_LEFT  = HidNpadButton_AnyLeft,
+    KEY_RIGHT = HidNpadButton_AnyRight,
+    KEY_SL    = HidNpadButton_AnySL,
+    KEY_SR    = HidNpadButton_AnySR,
+} HidControllerKeys;
+
+/// HidControllerJoystick \deprecated
+typedef enum DEPRECATED {
+    JOYSTICK_LEFT  = 0,
+    JOYSTICK_RIGHT = 1,
+
+    JOYSTICK_NUM_STICKS = 2,
+} HidControllerJoystick;
+
 /// HidDebugPadAttribute
 typedef enum {
     HidDebugPadAttribute_IsConnected   = BIT(0),    ///< IsConnected
@@ -354,6 +592,21 @@ typedef enum {
     HidGestureType_Pinch               = 8,    ///< Pinch
     HidGestureType_Rotate              = 9,    ///< Rotate
 } HidGestureType;
+
+/// HidControllerID \deprecated
+typedef enum DEPRECATED {
+    CONTROLLER_PLAYER_1 = 0,
+    CONTROLLER_PLAYER_2 = 1,
+    CONTROLLER_PLAYER_3 = 2,
+    CONTROLLER_PLAYER_4 = 3,
+    CONTROLLER_PLAYER_5 = 4,
+    CONTROLLER_PLAYER_6 = 5,
+    CONTROLLER_PLAYER_7 = 6,
+    CONTROLLER_PLAYER_8 = 7,
+    CONTROLLER_HANDHELD = 8,
+    CONTROLLER_UNKNOWN  = 9,
+    CONTROLLER_P1_AUTO = 10, ///< Not an actual HID-sysmodule ID. Only for hidKeys*()/hidJoystickRead()/hidSixAxisSensorValuesRead()/hidIsControllerConnected(). Automatically uses CONTROLLER_PLAYER_1 when connected, otherwise uses CONTROLLER_HANDHELD.
+} HidControllerID;
 
 /// GyroscopeZeroDriftMode
 typedef enum {
@@ -556,11 +809,37 @@ typedef enum {
     HidPalmaFeature_MuteSwitch                              = BIT(3),     ///< MuteSwitch
 } HidPalmaFeature;
 
+/// touchPosition \deprecated
+typedef struct DEPRECATED touchPosition {
+    u32 id;
+    u32 px;
+    u32 py;
+    u32 dx;
+    u32 dy;
+    u32 angle;
+} touchPosition;
+
 /// HidAnalogStickState
 typedef struct HidAnalogStickState {
     s32 x;                                    ///< X
     s32 y;                                    ///< Y
 } HidAnalogStickState;
+
+/// JoystickPosition \deprecated
+typedef struct DEPRECATED JoystickPosition {
+    s32 dx;
+    s32 dy;
+} JoystickPosition;
+
+/// MousePosition \deprecated
+typedef struct DEPRECATED MousePosition {
+    s32 x;
+    s32 y;
+    s32 velocityX;
+    s32 velocityY;
+    s32 scrollVelocityX;
+    s32 scrollVelocityY;
+} MousePosition;
 
 /// HidVector
 typedef struct HidVector {
@@ -573,6 +852,14 @@ typedef struct HidVector {
 typedef struct HidDirectionState {
     float direction[3][3];                      ///< 3x3 matrix
 } HidDirectionState;
+
+/// SixAxisSensorValues \deprecated DEPRECATED
+typedef struct DEPRECATED SixAxisSensorValues {
+    HidVector accelerometer;
+    HidVector gyroscope;
+    HidVector unk;
+    HidVector orientation[3];
+} SixAxisSensorValues;
 
 #define JOYSTICK_MAX (0x7FFF)
 #define JOYSTICK_MIN (-0x7FFF)
@@ -1149,17 +1436,33 @@ typedef struct HidPalmaActivityEntry {
     u16 wave_index;                         ///< WaveIndex
 } HidPalmaActivityEntry;
 
+static inline HidNpadIdType hidControllerIDToNpadIdType(HidControllerID id) {
+    if (id <= CONTROLLER_PLAYER_8) return (HidNpadIdType)id;
+    if (id == CONTROLLER_HANDHELD) return HidNpadIdType_Handheld;
+    return HidNpadIdType_Other;//For CONTROLLER_UNKNOWN and invalid values return this.
+}
+
+static inline HidControllerID hidControllerIDFromNpadIdType(HidNpadIdType id) {
+    if (id <= HidNpadIdType_No8) return (HidControllerID)id;
+    if (id == HidNpadIdType_Handheld) return CONTROLLER_HANDHELD;
+    return CONTROLLER_UNKNOWN;
+}
+
 /// Initialize hid. Called automatically during app startup.
 Result hidInitialize(void);
 
 /// Exit hid. Called automatically during app exit.
 void hidExit(void);
 
+void DEPRECATED hidReset(void); ///< \deprecated
+
 /// Gets the Service object for the actual hid service session.
 Service* hidGetServiceSession(void);
 
 /// Gets the address of the SharedMemory.
 void* hidGetSharedmemAddr(void);
+
+void DEPRECATED hidScanInput(void); ///< \deprecated
 
 ///@name TouchScreen
 ///@{
@@ -1428,6 +1731,39 @@ void hidInitializeGesture(void);
 size_t hidGetGestureStates(HidGestureState *states, size_t count);
 
 ///@}
+
+bool DEPRECATED hidIsControllerConnected(HidControllerID id); ///< \deprecated
+
+u64 DEPRECATED hidKeysHeld(HidControllerID id); ///< \deprecated
+u64 DEPRECATED hidKeysDown(HidControllerID id); ///< \deprecated
+u64 DEPRECATED hidKeysUp(HidControllerID id); ///< \deprecated
+
+u64 DEPRECATED hidMouseButtonsHeld(void); ///< \deprecated
+u64 DEPRECATED hidMouseButtonsDown(void); ///< \deprecated
+u64 DEPRECATED hidMouseButtonsUp(void); ///< \deprecated
+void DEPRECATED hidMouseRead(MousePosition *pos); ///< \deprecated
+u32 DEPRECATED hidMouseMultiRead(MousePosition *entries, u32 num_entries); ///< \deprecated
+
+bool DEPRECATED hidKeyboardModifierHeld(HidKeyboardModifier modifier); ///< \deprecated
+bool DEPRECATED hidKeyboardModifierDown(HidKeyboardModifier modifier); ///< \deprecated
+bool DEPRECATED hidKeyboardModifierUp(HidKeyboardModifier modifier); ///< \deprecated
+
+bool DEPRECATED hidKeyboardHeld(HidKeyboardScancode key); ///< \deprecated
+bool DEPRECATED hidKeyboardDown(HidKeyboardScancode key); ///< \deprecated
+bool DEPRECATED hidKeyboardUp(HidKeyboardScancode key); ///< \deprecated
+
+u32 DEPRECATED hidTouchCount(void); ///< \deprecated
+void DEPRECATED hidTouchRead(touchPosition *pos, u32 point_id); ///< \deprecated
+
+void DEPRECATED hidJoystickRead(JoystickPosition *pos, HidControllerID id, HidControllerJoystick stick); ///< \deprecated
+u32 DEPRECATED hidSixAxisSensorValuesRead(SixAxisSensorValues *values, HidControllerID id, u32 num_entries); ///< \deprecated
+
+/**
+ * @brief This can be used to check what CONTROLLER_P1_AUTO uses.
+ * @return 0 when CONTROLLER_PLAYER_1 is connected, otherwise returns 1 for handheld-mode.
+ * @deprecated
+ */
+bool DEPRECATED hidGetHandheldMode(void);
 
 /**
  * @brief SendKeyboardLockKeyEvent
@@ -2164,3 +2500,5 @@ Result hidSetTouchScreenConfiguration(const HidTouchScreenConfigurationForNx *co
  * @param[out] out Output flag.
  */
 Result hidIsFirmwareUpdateNeededForNotification(bool *out);
+
+#pragma GCC diagnostic pop
