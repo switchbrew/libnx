a09c8341030e77f938da6487ce184550953080c0
Michael Scire <SciresM@gmail.com>
2024-10-09 11:34:47 -0700

    svc: update WaitForAddress

--- nx/include/switch/kernel/svc.h
+++ nx/include/switch/kernel/svc.h
@@ -262,9 +262,10 @@ typedef enum {
 
 /// WaitForAddress behaviors.
 typedef enum {
-    ArbitrationType_WaitIfLessThan             = 0, ///< Wait if the value is less than argument.
-    ArbitrationType_DecrementAndWaitIfLessThan = 1, ///< Decrement the value and wait if it is less than argument.
-    ArbitrationType_WaitIfEqual                = 2, ///< Wait if the value is equal to argument.
+    ArbitrationType_WaitIfLessThan             = 0, ///< Wait if the 32-bit value is less than argument.
+    ArbitrationType_DecrementAndWaitIfLessThan = 1, ///< Decrement the 32-bit value and wait if it is less than argument.
+    ArbitrationType_WaitIfEqual                = 2, ///< Wait if the 32-bit value is equal to argument.
+    ArbitrationType_WaitIfEqual64              = 2, ///< [19.0.0+] Wait if the 64-bit value is equal to argument.
 } ArbitrationType;
 
 /// Context of a scheduled thread.
@@ -809,7 +810,7 @@ Result svcGetThreadContext3(ThreadContext* ctx, Handle thread);
  * @return Result code.
  * @note Syscall number 0x34.
  */
-Result svcWaitForAddress(void *address, u32 arb_type, s32 value, s64 timeout);
+Result svcWaitForAddress(void *address, u32 arb_type, s64 value, s64 timeout);
 
 /**
  * @brief Signals (and updates) an address depending on type and value. [4.0.0+]
