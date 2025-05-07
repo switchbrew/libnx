17bcd07095e8f45a207f4fd91a1e3ba796d1c00e
Michael Scire <SciresM@gmail.com>
2025-05-07 12:46:20 -0700

    fsp-pr: update RegisterProgram for 20.0.0 change

--- nx/include/switch/services/fspr.h
+++ nx/include/switch/services/fspr.h
@@ -18,7 +18,7 @@ void fsprExit(void);
 /// Gets the Service object for the actual fsp-pr service session.
 Service* fsprGetServiceSession(void);
 
-Result fsprRegisterProgram(u64 pid, u64 tid, NcmStorageId sid, const void *fs_access_header, size_t fah_size, const void *fs_access_control, size_t fac_size);
+Result fsprRegisterProgram(u64 pid, u64 tid, NcmStorageId sid, const void *fs_access_header, size_t fah_size, const void *fs_access_control, size_t fac_size, u8 fs_access_control_restriction_mode);
 Result fsprUnregisterProgram(u64 pid);
 Result fsprSetCurrentProcess(void);
 Result fsprSetEnabledProgramVerification(bool enabled);
