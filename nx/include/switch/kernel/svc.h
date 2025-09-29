bd75b9c072db2a5cc510d7cc3960debdd150c765
MasaGratoR <masagrator@gmail.com>
2025-09-29 15:42:06 +0200

    Update svcSetHeapSize note

--- nx/include/switch/kernel/svc.h
+++ nx/include/switch/kernel/svc.h
@@ -296,7 +296,7 @@ typedef enum {
 /**
  * @brief Set the process heap to a given size. It can both extend and shrink the heap.
  * @param[out] out_addr Variable to which write the address of the heap (which is randomized and fixed by the kernel)
- * @param[in] size Size of the heap, must be a multiple of 0x200000 and [2.0.0+] less than 0x100000000.
+ * @param[in] size Size of the heap, must be a multiple of 0x200000.
  * @return Result code.
  * @note Syscall number 0x01.
  */
