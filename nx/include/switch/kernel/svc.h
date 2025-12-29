03f956f7bb29310a7ca071fed9a4b55d738fbab6
MasaGratoR <masagrator@gmail.com>
2025-12-29 07:45:11 +0100

    Fix

--- nx/include/switch/kernel/svc.h
+++ nx/include/switch/kernel/svc.h
@@ -336,7 +336,7 @@ typedef enum {
 
 /// DebugEvent flags
 typedef enum {
-    DebugEventFlag_Stopped = BIT(1),
+    DebugEventFlag_Stopped = BIT(0),
 } DebugEventFlag;
 
 /// Address space types for CreateProcessFlags
