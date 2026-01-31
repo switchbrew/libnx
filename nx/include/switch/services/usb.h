d0709d80cddd79508208fe9601b91aa2564ef6f4
ndeadly <ndeadly@users.noreply.github.com>
2026-01-31 18:51:09 +0100

    usb: include additional definitions from libusb (#704)

--- nx/include/switch/services/usb.h
+++ nx/include/switch/services/usb.h
@@ -11,15 +11,26 @@
 
 /// Imported from libusb with changed names.
 /* Descriptor sizes per descriptor type */
-#define USB_DT_INTERFACE_SIZE        9
-#define USB_DT_ENDPOINT_SIZE         7
-#define USB_DT_DEVICE_SIZE         0x12
-#define USB_DT_SS_ENDPOINT_COMPANION_SIZE 6
+#define USB_DT_INTERFACE_SIZE             0x09
+#define USB_DT_ENDPOINT_SIZE              0x07
+#define USB_DT_INTERFACE_ASSOCIATION_SIZE 0x08
+#define USB_DT_DEVICE_SIZE                0x12
+#define USB_DT_HID_SIZE                   0x09
+#define USB_DT_SS_ENDPOINT_COMPANION_SIZE 0x06
 
 #define USB_ENDPOINT_ADDRESS_MASK 0x0f    /* in bEndpointAddress */
 #define USB_ENDPOINT_DIR_MASK 0x80
 
 #define USB_TRANSFER_TYPE_MASK 0x03 /* in bmAttributes */
+
+/// Imported from libusb, with some adjustments.
+struct usb_control_setup {
+    uint8_t  bmRequestType;
+    uint8_t  bRequest;
+    uint16_t wValue;
+    uint16_t wIndex;
+    uint16_t wLength;
+};
 
 /// Imported from libusb, with some adjustments.
 struct usb_endpoint_descriptor {
@@ -90,6 +101,29 @@ struct usb_string_descriptor {
     uint16_t wData[0x40];
 };
 
+/// Imported from libusb, with some adjustments.
+struct usb_interface_association_descriptor {
+    uint8_t bLength;
+    uint8_t bDescriptorType; ///< Must match USB_DT_INTERFACE_ASSOCIATION.
+    uint8_t bFirstInterface;
+    uint8_t bInterfaceCount;
+    uint8_t bFunctionClass;
+    uint8_t bFunctionSubClass;
+    uint8_t bFunctionProtocol;
+    uint8_t iFunction;
+};
+
+/// Imported from libusb, with some adjustments.
+struct usb_hid_descriptor {
+    uint8_t  bLength;
+    uint8_t  bDescriptorType; ///< Must match USB_DT_HID.
+    uint16_t bcdHID;
+    uint8_t  bCountryCode;
+    uint8_t  bNumDescriptors;
+    uint8_t  bClassDescriptorType;
+    uint16_t wClassDescriptorLength;
+} NX_PACKED;
+
 /// Imported from libusb, with changed names.
 enum usb_class_code {
     USB_CLASS_PER_INTERFACE = 0,
@@ -120,6 +154,7 @@ enum usb_descriptor_type {
     USB_DT_STRING = 0x03,
     USB_DT_INTERFACE = 0x04,
     USB_DT_ENDPOINT = 0x05,
+    USB_DT_INTERFACE_ASSOCIATION = 0x0b,
     USB_DT_BOS = 0x0f,
     USB_DT_DEVICE_CAPABILITY = 0x10,
     USB_DT_HID = 0x21,
@@ -190,6 +225,22 @@ enum usb_standard_request {
     /** Delay from the time a host transmits a packet to the time it is
      * received by the device. */
     USB_SET_ISOCH_DELAY = 0x31,
+};
+
+/// Imported from libusb, with changed names.
+enum usb_request_type {
+    USB_REQUEST_TYPE_STANDARD = (0x00 << 5),
+    USB_REQUEST_TYPE_CLASS = (0x01 << 5),
+    USB_REQUEST_TYPE_VENDOR = (0x02 << 5),
+    USB_REQUEST_TYPE_RESERVED = (0x03 << 5)
+};
+
+/// Imported from libusb, with changed names.
+enum usb_request_recipient {
+    USB_RECIPIENT_DEVICE = 0x00,
+    USB_RECIPIENT_INTERFACE = 0x01,
+    USB_RECIPIENT_ENDPOINT = 0x02,
+    USB_RECIPIENT_OTHER = 0x03
 };
 
 /// Imported from libusb, with changed names.
