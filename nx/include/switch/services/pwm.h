ea97c980a4915f72c8e1d1e1559476e37123c4d8
MasaGratoR <masagrator@gmail.com>
2026-02-07 00:54:11 +0100

    Update pwm.h

--- nx/include/switch/services/pwm.h
+++ nx/include/switch/services/pwm.h
@@ -14,9 +14,9 @@ typedef struct {
 } PwmChannelSession;
 
 typedef enum {
-    PwmChannelDeviceCode_CpuFan = 0x3D000001,
+    PwmChannelDeviceCode_CpuFan       = 0x3D000001,
     PwmChannelDeviceCode_LcdBacklight = 0x3400003D,
-    PwmChannelDeviceCode_Led = 0x35000065
+    PwmChannelDeviceCode_Led          = 0x35000065
 } PwmChannelDeviceCode;
 
 /// Initialize pwm.
@@ -35,4 +35,5 @@ Result pwmOpenSession2(PwmChannelSession *out, PwmChannelDeviceCode device_code)
 Result pwmChannelSessionGetDutyCycle(PwmChannelSession *c, double* out);
 
 /// Closes a PwmChannelSession.
+
 void pwmChannelSessionClose(PwmChannelSession *c);
