640304ed719ade78afcd4f05264d55ff8bf5f801
MasaGratoR <masagrator@gmail.com>
2026-02-07 00:48:33 +0100

    Add files via upload

---
+++ nx/source/services/pwm.c
@@ -0,0 +1,46 @@
+#define NX_SERVICE_ASSUME_NON_DOMAIN
+#include "service_guard.h"
+#include "services/pwm.h"
+
+static Service g_pwmSrv;
+
+NX_GENERATE_SERVICE_GUARD(pwm);
+
+Result _pwmInitialize(void) {
+    return smGetService(&g_pwmSrv, "pwm");
+}
+
+void _pwmCleanup(void) {
+    serviceClose(&g_pwmSrv);
+}
+
+Service* pwmGetServiceSession(void) {
+    return &g_pwmSrv;
+}
+
+Result pwmOpenSession2(PwmChannelSession *out, PwmChannelDeviceCode device_code) {
+
+    if (hosversionBefore(6,0,0))
+        return MAKERESULT(Module_Libnx, LibnxError_IncompatSysVer);
+
+    u32 tmp = device_code;
+
+    return serviceDispatchIn(&g_pwmSrv, 2, tmp,
+        .out_num_objects = 1,
+        .out_objects = &out->s,
+    );
+}
+
+Result pwmChannelSessionGetDutyCycle(PwmChannelSession *c, double* out) {
+    if (hosversionBefore(6,0,0))
+        return MAKERESULT(Module_Libnx, LibnxError_IncompatSysVer);
+
+    if (!serviceIsActive(c->s))
+        return MAKERESULT(Module_Libnx, LibnxError_NotInitialized);
+
+    return serviceDispatchOut(&c->s, 7, *out);
+}
+
+void pwmChannelSessionClose(PwmChannelSession *c) {
+    serviceClose(&c->s);
+}
