7ecc8aac760a803f1b597213ea1c571087e6512a
MasaGratoR <masagrator@gmail.com>
2026-02-15 19:22:51 +0100

    Update switch.h

--- nx/include/switch.h
+++ nx/include/switch.h
@@ -132,6 +132,7 @@ extern "C" {
 #include "switch/services/ectx.h"
 #include "switch/services/avm.h"
 #include "switch/services/mm.h"
+#include "switch/services/omm.h"
 
 #include "switch/display/binder.h"
 #include "switch/display/parcel.h"
