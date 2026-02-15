0f3b10f1b96e48e8dcfa7913ade0545f938f653e
MasaGratoR <masagrator@gmail.com>
2026-02-15 19:22:22 +0100

    Add files via upload

--- nx/include/switch/services/omm.h
+++ nx/include/switch/services/omm.h
@@ -11,12 +11,18 @@
 
 ///OperationModePolicy
 typedef enum {
+    ommOperationMode_Handheld   = 0,
+    ommOperationMode_Console    = 1
+} ommOperationMode;
+
+///OperationModePolicy
+typedef enum {
     ommOperationModePolicy_Auto       = 0,
     ommOperationModePolicy_Handheld   = 1,
     ommOperationModePolicy_Console    = 2
 } ommOperationModePolicy;
 
-/// Initialize pwm.
+/// Initialize omm.
 Result ommInitialize(void);
 
 /// Exit omm.
@@ -27,6 +33,9 @@ Service* ommGetServiceSession(void);
 
 /// Returns a DefaultDisplayResolution. Only available on [3.0.0+].
 Result ommGetDefaultDisplayResolution(s32* width, s32* height);
+
+/// Returns an ommOperationModePolicy.
+Result ommGetOperationMode(ommOperationMode* s);
 
 /// Takes an ommOperationModePolicy. Only available on [3.0.0+].
 Result ommSetOperationModePolicy(ommOperationModePolicy value);
