f3b59088178f8960c95bab7ffeaed43709ccc4ba
MasaGratoR <masagrator@gmail.com>
2026-04-26 00:52:24 +0200

    Add idlesys.h

---
+++ nx/include/switch/services/idlesys.h
@@ -0,0 +1,21 @@
+/**
+ * @file idlesys.h
+ * @author MasaGratoR
+ * @copyright libnx Authors
+ */
+
+#pragma once
+
+#include "../types.h"
+
+/// Initialize idle:sys.
+Result idlesysInitialize(void);
+
+/// Exit idle:sys.
+void idlesysExit(void);
+
+/// Gets the Service for idle:sys.
+Service* idlesysGetServiceSession(void);
+
+/// Resets sleep counter.
+Result idlesysReportUserIsActive();
