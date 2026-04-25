f8634a8cf3e485954380d723a47c5ac9674baae9
MasaGratoR <masagrator@gmail.com>
2026-04-26 00:52:51 +0200

    Add idlesys.c

---
+++ nx/source/services/idlesys.c
@@ -0,0 +1,23 @@
+#define NX_SERVICE_ASSUME_NON_DOMAIN
+#include "service_guard.h"
+#include "services/idlesys.h"
+
+static Service g_idlesysSrv;
+
+NX_GENERATE_SERVICE_GUARD(idlesys);
+
+Result _idlesysInitialize(void) {
+    return smGetService(&g_idlesysSrv, "idle:sys");
+}
+
+void _idlesysCleanup(void) {
+    serviceClose(&g_idlesysSrv);
+}
+
+Service* pwmGetServiceSession(void) {
+    return &g_idlesysSrv;
+}
+
+Result idlesysReportUserIsActive() {
+    return serviceDispatch(&g_idlesysSrv, 5);
+}
