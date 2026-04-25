b999a18f75d2c845b6e69108abc65de969459a08
MasaGratoR <masagrator@gmail.com>
2026-04-25 10:36:51 +0200

    Match struct name to nnSdk

--- nx/source/services/nifm.c
+++ nx/source/services/nifm.c
@@ -217,7 +217,7 @@ Result nifmGetCurrentNetworkProfile(NifmNetworkProfileData *profile) {
     return rc;
 }
 
-Result nifmEnumerateNetworkProfiles(NifmNetworkProfileGroup group, NifmNetworkProfile* buffer, size_t max_entries, u32* total_entries) {
+Result nifmEnumerateNetworkProfiles(NifmNetworkProfileGroup group, NifmNetworkProfileBasicInfo* buffer, size_t max_entries, u32* total_entries) {
     u8 in = (u8)group;
     serviceAssumeDomain(&g_nifmIGS);
     return serviceDispatchInOut(&g_nifmIGS, 7, in, *total_entries,
